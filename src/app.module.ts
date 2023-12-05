6cf540965db015696bf6fb3413ac22d9bc027e6d
mlucasdev <lucasmsantos819@gmail.com>
2023-12-04 21:54:15 -0300

    fix: remove imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
