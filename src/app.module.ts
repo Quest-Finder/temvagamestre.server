7c4dcdb881c1eac89f70484acbe7e7ea11d25e7c
mlucasdev <lucasmsantos819@gmail.com>
2023-12-04 21:22:45 -0300

    chore: remove app service and app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
