d82d58a274ec64431ff18b3ca8fe03417fd05748
mlucasdev <lucasmsantos819@gmail.com>
2023-12-08 17:40:05 -0300

    feat: ensure integration between AddUserUseCase and IdBuilder

--- src/usecases/implementations/user/add-user/add-user-usecase.ts
+++ src/usecases/implementations/user/add-user/add-user-usecase.ts
@@ -1,16 +1,21 @@
 import type { AddUserData, AddUserResponse, AddUser } from '@/domain/contracts/user'
 import type { FindUserByEmailRepo } from '@/usecases/contracts/db/user'
+import type { IdBuilder } from '@/usecases/contracts/id'
 import { EmailInUseError } from '@/domain/errors'
 import { left } from '@/shared/either'
 
 export class AddUserUseCase implements AddUser {
-  constructor (private readonly findUserByEmailRepo: FindUserByEmailRepo) {}
+  constructor (
+    private readonly findUserByEmailRepo: FindUserByEmailRepo,
+    private readonly idBuilder: IdBuilder
+  ) {}
 
   async perform (data: AddUserData): Promise<AddUserResponse> {
     const userOrNull = await this.findUserByEmailRepo.execute(data.email)
     if (userOrNull) {
       return left(new EmailInUseError(data.email))
     }
+    this.idBuilder.build()
     return '' as any
   }
 }
