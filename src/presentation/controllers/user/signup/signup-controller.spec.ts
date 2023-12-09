a5fef3ebfb14967c59320125174f8e9d0aab59ff
mlucasdev <lucasmsantos819@gmail.com>
2023-12-09 15:50:24 -0300

    test: ensure integration between SignUpController and AddUser

--- src/presentation/controllers/user/signup/signup-controller.spec.ts
+++ src/presentation/controllers/user/signup/signup-controller.spec.ts
@@ -1,8 +1,8 @@
+import type { AddUser, AddUserData, AddUserResponse } from '@/domain/contracts/user'
 import type { Validation } from '@/presentation/contracts'
 import type { HttpRequest } from '@/presentation/types/http'
-import { ServerError } from '@/presentation/errors'
-import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
-import { type Either, right, left } from '@/shared/either'
+import { badRequest, noContent, serverError } from '@/presentation/helpers/http-helpers'
+import { left, right, type Either } from '@/shared/either'
 import { SignUpController } from './signup-controller'
 import MockDate from 'mockdate'
 
@@ -25,15 +25,26 @@ const makeValidation = (): Validation => {
   return new ValidationStub()
 }
 
+const makeAddUser = (): AddUser => {
+  class AddUserStub implements AddUser {
+    async perform (account: AddUserData): Promise<AddUserResponse> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new AddUserStub()
+}
+
 interface SutTypes {
   sut: SignUpController
   validationStub: Validation
+  addUserStub: AddUser
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidation()
-  const sut = new SignUpController(validationStub)
-  return { sut, validationStub }
+  const addUserStub = makeAddUser()
+  const sut = new SignUpController(validationStub, addUserStub)
+  return { sut, validationStub, addUserStub }
 }
 
 describe('SignUpController', () => {
@@ -59,12 +70,43 @@ describe('SignUpController', () => {
 
   it('Should return 500 if Validation throws', async () => {
     const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockImplementationOnce(() => {
-      throw new Error()
-    })
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
     const httpResponse = await sut.handle(makeFakeRequest())
     const error = new Error()
     error.stack = 'any_stack'
-    expect(httpResponse).toEqual(serverError(new ServerError(error.stack)))
+    expect(httpResponse).toEqual(serverError())
+  })
+
+  it('Should call AddUser with correct values', async () => {
+    const { sut, addUserStub } = makeSut()
+    const performSpy = jest.spyOn(addUserStub, 'perform')
+    await sut.handle(makeFakeRequest())
+    expect(performSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+
+  it('Should return 400 if AddUser return an Error', async () => {
+    const { sut, addUserStub } = makeSut()
+    jest.spyOn(addUserStub, 'perform').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if AddUser throws', async () => {
+    const { sut, addUserStub } = makeSut()
+    jest.spyOn(addUserStub, 'perform').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError())
+  })
+
+  it('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
