edbd89ce789262426a5fca20b65043112a7cfb3b
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 11:42:30 -0300

    feat: ensure cors middleware enables CORS

--- src/main/configs/app.ts
+++ src/main/configs/app.ts
@@ -1,9 +1,12 @@
 import { type INestApplication } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '../app.module'
+import { cors } from '../middlewares'
 
 const createApp = async (): Promise<INestApplication<any>> => {
-  return await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule)
+  app.use(cors)
+  return app
 }
 
 export default createApp
