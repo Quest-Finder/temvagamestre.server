b6703ae0af45a4137017d27916963a77a6bd3c0a
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 13:56:59 -0300

    test: create a new test route with getHttpAdapter()

--- src/main/middlewares/cors/cors.test.ts
+++ src/main/middlewares/cors/cors.test.ts
@@ -4,6 +4,9 @@ import createApp from '@/main/configs/app'
 describe('CORS Middleware', () => {
   it('Should enable CORS', async () => {
     const app = await createApp()
+    app.getHttpAdapter().get('/test_cors', (req, res) => {
+      res.send()
+    })
     await request(app.getHttpServer())
       .get('/test_cors')
       .expect('access-control-allow-origin', '*')
