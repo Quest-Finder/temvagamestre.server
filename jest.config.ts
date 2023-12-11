731409ce12f96d4c0eaf9c32d8b04dfe325baeb2
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 20:57:45 -0300

    chore: add collectCoverageFrom

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/shared/either.ts',
+    '!<rootDir>/src/main/{configs,adapters}/**/*.ts',
+    '!<rootDir>/src/main/main.ts',
+  ],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
   },
