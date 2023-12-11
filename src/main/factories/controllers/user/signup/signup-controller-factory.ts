1daf9253e99b62301dbe2692bd7c8b4a9ec6e1d2
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 16:30:45 -0300

    fix: change Controller type

--- src/main/factories/controllers/user/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/user/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { makeAddUserUseCase } from '@/main/factories/usecases/user/add-user-usecase-factory'
 import { SignUpController } from '@/presentation/controllers/user/signup/signup-controller'
 import { SignUpZodValidation } from '@/validators/user/signup/signup-zod-validation'
-import { type Controller } from '@nestjs/common/interfaces'
+import { type Controller } from '@/presentation/contracts/controller'
 
 export const makeSignUpController = (): Controller => {
   const validation = new SignUpZodValidation()
