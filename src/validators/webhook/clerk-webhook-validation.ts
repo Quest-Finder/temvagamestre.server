f1ab9879d487e5c475eb93abfb71e5e76fa8f1ec
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 19:47:54 -0300

    feat: ensure ClerkWebhookValidation return right resutl if Svix Webhook verify is a success

--- src/validators/webhook/clerk-webhook-validation.ts
+++ src/validators/webhook/clerk-webhook-validation.ts
@@ -1,14 +1,23 @@
+import env from '@/main/configs/env'
 import type { Validation } from '@/presentation/contracts'
-import { right, type Either, left } from '@/shared/either'
+import { left, right, type Either } from '@/shared/either'
+import { Webhook } from 'svix'
+import { InvalidSvixError } from '../errors'
 import { type SvixHeaders } from '../types/svix-headers'
-import { InvalidSvixError } from '../errors/invalid-svix-headers'
 
 export class ClerkWebhookValidation implements Validation {
   async validate (input: any): Promise<Either<Error, null>> {
     const headers = input.headers as SvixHeaders
+    const payload = input.body
     if (!headers?.['svix-id'] || !headers['svix-signature'] || !headers['svix-timestamp']) {
       return left(new InvalidSvixError())
     }
+    const wh = new Webhook(env.webhookSecret)
+    wh.verify(payload, {
+      'svix-id': headers['svix-id'],
+      'svix-timestamp': headers['svix-timestamp'],
+      'svix-signature': headers['svix-signature']
+    })
     return right(null)
   }
 }
