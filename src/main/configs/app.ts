0e5ee38298d5c07e99a9fa9468517e45947a7441
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 13:59:26 -0300

    feat: ensure http request parser body as json

--- src/main/configs/app.ts
+++ src/main/configs/app.ts
@@ -2,10 +2,12 @@ import { type INestApplication } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '../app.module'
 import { cors } from '../middlewares'
+import { bodyParser } from '../middlewares/body-parser/body-parser'
 
 const createApp = async (): Promise<INestApplication<any>> => {
   const app = await NestFactory.create(AppModule)
   app.use(cors)
+  app.use(bodyParser)
   return app
 }
 
