7afd26ec470a834020f94368f4162a46696c4775
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 19:48:12 -0300

    test: ensure ClerkWebhookValidation return right resutl if Svix Webhook verify is a success

--- src/validators/webhook/clerk-webhook-validation.spec.ts
+++ src/validators/webhook/clerk-webhook-validation.spec.ts
@@ -1,14 +1,22 @@
-import { InvalidSvixError } from '../errors/invalid-svix-headers'
+import type { HttpRequest } from '@/presentation/types/http'
+import { Webhook } from 'svix'
+import { InvalidSvixError } from '../errors/invalid-svix-headers-error'
 import { ClerkWebhookValidation } from './clerk-webhook-validation'
 
-// const makeFakeRequest = (): HttpRequest => ({
-//   headers: {
-//     'svix-id': 'any_svix_id',
-//     'svix-timestamp': 'any_svix_timestamp',
-//     'svix-signature': 'any_svix_signature'
-//   },
-//   body: { payload: 'any_payload' }
-// })
+jest.mock('svix', () => ({
+  Webhook: jest.fn(() => ({
+    verify: jest.fn(() => true)
+  }))
+}))
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'svix-id': 'any_svix_id',
+    'svix-timestamp': 'any_svix_timestamp',
+    'svix-signature': 'any_svix_signature'
+  },
+  body: { payload: 'any_payload' }
+})
 
 const makeSut = (): ClerkWebhookValidation => {
   return new ClerkWebhookValidation()
@@ -18,5 +26,17 @@ describe('ClerkWebhookValidation', () => {
     const sut = makeSut()
     const result = await sut.validate({})
     expect(result.value).toEqual(new InvalidSvixError())
+  })
+
+  it('Should call Svix Webhook with correct secret', async () => {
+    const sut = makeSut()
+    await sut.validate(makeFakeRequest())
+    expect(Webhook).toHaveBeenCalledWith('any_secret')
+  })
+
+  it('Should return right resutl if Svix Webhook verify is a success', async () => {
+    const sut = makeSut()
+    const result = await sut.validate(makeFakeRequest())
+    expect(result.isRight()).toBe(true)
   })
 })
