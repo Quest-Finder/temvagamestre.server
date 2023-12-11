511a6d9127c6e26acaa2f46954cd824a596c55aa
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 15:06:32 -0300

    refactor: remove bodyParser

--- src/main/configs/app.ts
+++ src/main/configs/app.ts
@@ -1,12 +1,9 @@
 import { type INestApplication } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '../app.module'
-import { bodyParser } from '../middlewares/body-parser/body-parser'
 
 const createApp = async (): Promise<INestApplication<any>> => {
-  const app = await NestFactory.create(AppModule)
-  app.use(bodyParser)
-  return app
+  return await NestFactory.create(AppModule)
 }
 
 export default createApp
