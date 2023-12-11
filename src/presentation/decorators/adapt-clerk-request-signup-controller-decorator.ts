1fc4cdf84da1e7ff2fdd8bc58ed206bb08ac66e4
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 17:37:18 -0300

    feat: ensure AdaptClerkRequestSignUpControllerDecorator call Controller with the formatted request body

---
+++ src/presentation/decorators/adapt-clerk-request-signup-controller-decorator.ts
@@ -0,0 +1,22 @@
+import type { HttpRequest, HttpResponse } from '../types/http'
+import type { Controller } from '../contracts'
+import type { ClerkSignUpEventData } from '../types/external/clerk-signup-request'
+import type { AddUserData } from '@/domain/contracts/user'
+
+export class AdaptClerkRequestSignUpControllerDecorator implements Controller {
+  constructor (private readonly controller: Controller) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const clerkRequest = httpRequest.body as ClerkSignUpEventData
+    const formatedRequest: AddUserData = {
+      externalAuthUserId: clerkRequest.data.id,
+      firstName: clerkRequest.data.first_name,
+      lastName: clerkRequest.data.last_name,
+      email: clerkRequest.data.email_addresses[0].email_address,
+      ...(clerkRequest.data.phone_numbers.length && { phone: clerkRequest.data.phone_numbers[0] })
+    }
+    httpRequest.body = formatedRequest
+    await this.controller.handle(httpRequest)
+    return { statusCode: 0, body: '' }
+  }
+}
--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,11 @@
 import type { HttpResponse } from '@/presentation/types/http'
 import { ServerError } from '@/presentation/errors/server-error'
 
+export const ok = (body: any): HttpResponse => ({
+  statusCode: 200,
+  body
+})
+
 export const noContent = (): HttpResponse => ({
   statusCode: 204,
   body: null
