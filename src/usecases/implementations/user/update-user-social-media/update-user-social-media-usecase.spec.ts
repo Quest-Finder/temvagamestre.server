ca03e3fcf397f1d5a82a916acb69499321aa9780
Natalia Perri <lnataliaperri@gmail.com>
2023-12-12 19:03:46 -0300

    test: ensure UpdateUserSocialMediaUseCasa throws if FindSocialMediaByIdRepo throws

--- src/usecases/implementations/user/update-user-social-media/update-user-social-media-usecase.spec.ts
+++ src/usecases/implementations/user/update-user-social-media/update-user-social-media-usecase.spec.ts
@@ -2,6 +2,7 @@ import { type FindSocialMediaByIdRepo } from '@/usecases/contracts/db/social-med
 import { UpdateUserSocialMediaUseCase } from './update-user-social-media-usecase'
 import { type UpdateUserSocialMediaData } from '@/domain/contracts/user'
 import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
+import { InvalidSocialMediaIdError } from '@/domain/errors'
 
 const makeFakeSocialMediaData = (): UpdateUserSocialMediaData => ({
   externalAuthUserId: 'any_user_id',
@@ -9,10 +10,15 @@ const makeFakeSocialMediaData = (): UpdateUserSocialMediaData => ({
   link: 'any_link'
 })
 
+const makeFakeSocialMediaModel = (): SocialMediaModel => ({
+  id: 'any_social_media_id',
+  name: 'any_name'
+})
+
 const makeFindSocialMediaByIdRepo = (): FindSocialMediaByIdRepo => {
   class FindSocialMediaByIdRepoStud implements FindSocialMediaByIdRepo {
     async execute (socialMediaId: string): Promise<null | SocialMediaModel> {
-      return await Promise.resolve(null)
+      return await Promise.resolve(makeFakeSocialMediaModel())
     }
   }
   return new FindSocialMediaByIdRepoStud()
@@ -40,5 +46,12 @@ describe('UpdateUserSocialMediaUseCase', () => {
     await sut.perform(makeFakeSocialMediaData())
     expect(executeSpy).toHaveBeenCalledTimes(1)
     expect(executeSpy).toHaveBeenCalledWith('any_social_media_id')
+  })
+
+  it('Should return InvalidSocialMediaIdError if FindSocialMediaByIdRepo returns null', async () => {
+    const { sut, findSocialMediaByIdRepoStub } = makeSut()
+    jest.spyOn(findSocialMediaByIdRepoStub, 'execute').mockReturnValueOnce(Promise.resolve(null))
+    const result = await sut.perform(makeFakeSocialMediaData())
+    expect(result.value).toEqual(new InvalidSocialMediaIdError('any_social_media_id'))
   })
 })
