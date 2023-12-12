c9055d2a6cccaa6f34214d7648a50b76f6cd5bdd
mlucasdev <lucasmsantos819@gmail.com>
2023-12-12 08:44:37 -0300

    chore: add port and host env variable to app.listen()

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -3,5 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  webhookSecret: process.env.WEBHOOK_SECRET ?? 'any_webhook_secret'
+  webhookSecret: process.env.WEBHOOK_SECRET ?? 'any_webhook_secret',
+  serverPort: process.env.SERVER_PORT ?? '5050',
+  serverHost: process.env.SERVER_HOST ?? 'localhost'
 }
