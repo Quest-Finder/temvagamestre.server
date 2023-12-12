0c091f639d77a883ad321c96bb37b91a72150b08
Natalia Perri <lnataliaperri@gmail.com>
2023-12-12 18:59:21 -0300

    test: ensure FindSocialMediaByIdRepo calls with correct social media id

---
+++ src/usecases/implementations/user/update-user-social-media/update-user-social-media-usecase.spec.ts
@@ -0,0 +1,44 @@
+import { type FindSocialMediaByIdRepo } from '@/usecases/contracts/db/social-media/find-social-media-by-id-repo'
+import { UpdateUserSocialMediaUseCase } from './update-user-social-media-usecase'
+import { type UpdateUserSocialMediaData } from '@/domain/contracts/user'
+import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
+
+const makeFakeSocialMediaData = (): UpdateUserSocialMediaData => ({
+  externalAuthUserId: 'any_user_id',
+  socialMediaId: 'any_social_media_id',
+  link: 'any_link'
+})
+
+const makeFindSocialMediaByIdRepo = (): FindSocialMediaByIdRepo => {
+  class FindSocialMediaByIdRepoStud implements FindSocialMediaByIdRepo {
+    async execute (socialMediaId: string): Promise<null | SocialMediaModel> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new FindSocialMediaByIdRepoStud()
+}
+
+interface SutTypes {
+  sut: UpdateUserSocialMediaUseCase
+  findSocialMediaByIdRepoStub: FindSocialMediaByIdRepo
+}
+
+const makeSut = (): SutTypes => {
+  const findSocialMediaByIdRepoStub = makeFindSocialMediaByIdRepo()
+  const sut = new UpdateUserSocialMediaUseCase(findSocialMediaByIdRepoStub)
+
+  return {
+    sut,
+    findSocialMediaByIdRepoStub
+  }
+}
+
+describe('UpdateUserSocialMediaUseCase', () => {
+  it('Should call FindSocialMediaByIdRepo with correct social media id', async () => {
+    const { sut, findSocialMediaByIdRepoStub } = makeSut()
+    const executeSpy = jest.spyOn(findSocialMediaByIdRepoStub, 'execute')
+    await sut.perform(makeFakeSocialMediaData())
+    expect(executeSpy).toHaveBeenCalledTimes(1)
+    expect(executeSpy).toHaveBeenCalledWith('any_social_media_id')
+  })
+})
