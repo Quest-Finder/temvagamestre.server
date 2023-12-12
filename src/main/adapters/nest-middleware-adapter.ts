bf1f019db8c52331bd3d71eaac0e58e1eddffcd4
mlucasdev <lucasmsantos819@gmail.com>
2023-12-11 21:10:00 -0300

    feat: create NestMidlewareAdapter

---
+++ src/main/adapters/nest-middleware-adapter.ts
@@ -0,0 +1,25 @@
+import type { Controller } from '@/presentation/contracts'
+import type { HttpRequest } from '@/presentation/types/http'
+import type { Response, Request, NextFunction } from 'express'
+
+export class NestMidlewareAdapter {
+  constructor (private readonly controller: Controller) {}
+
+  async adapt (req: Request, res: Response, next: NextFunction): Promise<void> {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      headers: req.headers,
+      params: req.params
+    }
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        name: httpResponse.body.name,
+        error: httpResponse.body.message,
+        statusCode: httpResponse.statusCode
+      })
+    }
+  }
+}
