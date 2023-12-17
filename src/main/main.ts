b434c3f2d0811e5b3395fbe8510ab6150250716e
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2023-12-16 21:00:56 -0300

    fix: PORT

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT)
+  await app.listen(process.env.PORT|| 3000)
   console.log(`Server running`)
 }
 
