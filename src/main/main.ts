c5209346501e009623c50cc109b94507476dc9ce
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2023-12-16 21:09:46 -0300

    fix config port

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT|| 3000)
-  console.log(`Server running`)
+  const port = app.get(ConfigService).get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 
 bootstrap()
