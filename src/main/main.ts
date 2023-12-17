52412012a63eaad33a1cd986e53ffe673252c12b
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2023-12-16 21:52:27 -0300

    fix: port problems

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import dotenv from "dotenv";
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Server running`);
+  await app.listen(PORT);
+  console.log(`Server running smoothly...`);
 }
 
 bootstrap().catch(console.error);
