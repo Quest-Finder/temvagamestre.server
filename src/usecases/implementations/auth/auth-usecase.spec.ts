fd78f577678b85c12fe6b190ec365d1d07282703
mlucasdev <lucasmsantos819@gmail.com>
2023-12-18 11:21:04 -0300

    test: ensure AuthUseCase return an user id on success

--- src/usecases/implementations/auth/auth-usecase.spec.ts
+++ src/usecases/implementations/auth/auth-usecase.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidTokenError } from '@/domain/errors'
+import { AccessDeniedError, InvalidTokenError } from '@/domain/errors'
 import type { Decrypter } from '@/usecases/contracts/cryptography/decrypter'
 import { AuthUseCase } from './auth-usecase'
 import { type FindExternalAuthMappingByExternalAuthUserIdRepo } from '@/usecases/contracts/db/external-auth-mapping'
@@ -12,7 +12,7 @@ const makeFakeExternalAuthMappingModel = (): ExternalAuthMappingModel => ({
 const makeDecrypter = (): Decrypter => {
   class DecrypterStub implements Decrypter {
     async execute (token: string): Promise<null | string> {
-      return await Promise.resolve('any_id')
+      return await Promise.resolve('any_external_auth_user_id')
     }
   }
   return new DecrypterStub()
@@ -75,6 +75,30 @@ describe('AuthUseCase', () => {
     const executeSpy = jest.spyOn(findExternalAuthMappingByExternalAuthUserIdRepoStub, 'execute')
     await sut.perform('any_token')
     expect(executeSpy).toHaveBeenCalledTimes(1)
-    expect(executeSpy).toHaveBeenCalledWith('any_id')
+    expect(executeSpy).toHaveBeenCalledWith('any_external_auth_user_id')
+  })
+
+  it('Should return AccessDeniedError if FindExternalAuthMappingByExternalAuthUserIdRepo returns null', async () => {
+    const { sut, findExternalAuthMappingByExternalAuthUserIdRepoStub } = makeSut()
+    jest.spyOn(findExternalAuthMappingByExternalAuthUserIdRepoStub, 'execute').mockReturnValueOnce(
+      Promise.resolve(null)
+    )
+    const result = await sut.perform('any_token')
+    expect(result.value).toEqual(new AccessDeniedError())
+  })
+
+  it('Should throw if FindExternalAuthMappingByExternalAuthUserIdRepo throws', async () => {
+    const { sut, findExternalAuthMappingByExternalAuthUserIdRepoStub } = makeSut()
+    jest.spyOn(findExternalAuthMappingByExternalAuthUserIdRepoStub, 'execute').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const promise = sut.perform('any_token')
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('Should return an user id on success', async () => {
+    const { sut } = makeSut()
+    const result = await sut.perform('any_token')
+    expect(result.value).toEqual({ userId: 'any_user_id' })
   })
 })
