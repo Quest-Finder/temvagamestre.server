27523668d3e8a6923e41d1b3bad0ec77eb23995e
mlucasdev <lucasmsantos819@gmail.com>
2023-12-18 10:30:10 -0300

    feat: ensure AuthUseCase call FindExternalAuthMapping with correct value

--- src/usecases/implementations/auth/auth-usecase.ts
+++ src/usecases/implementations/auth/auth-usecase.ts
@@ -1,16 +1,21 @@
+import type { FindExternalAuthMappingByExternalAuthUserIdRepo } from '@/usecases/contracts/db/external-auth-mapping'
 import type { AuthResponse, Auth } from '@/domain/contracts/user'
 import type { Decrypter } from '@/usecases/contracts/cryptography/decrypter'
 import { left, right } from '@/shared/either'
 import { InvalidTokenError } from '@/domain/errors'
 
 export class AuthUseCase implements Auth {
-  constructor (private readonly descrypter: Decrypter) {}
+  constructor (
+    private readonly descrypter: Decrypter,
+    private readonly findExternalAuthMappingByExternalAuthUserIdRepo: FindExternalAuthMappingByExternalAuthUserIdRepo
+  ) {}
 
   async perform (token: string): Promise<AuthResponse> {
-    const value = await this.descrypter.execute(token)
-    if (!value) {
+    const externalAuthUserId = await this.descrypter.execute(token)
+    if (!externalAuthUserId) {
       return left(new InvalidTokenError())
     }
+    await this.findExternalAuthMappingByExternalAuthUserIdRepo.execute(externalAuthUserId)
     return right({ userId: '' })
   }
 }
