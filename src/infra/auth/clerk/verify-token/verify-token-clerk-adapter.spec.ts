c0cf3839ea688197d042c5840a24097eb8386975
mlucasdev <lucasmsantos819@gmail.com>
2023-12-19 14:50:40 -0300

    test: ensure integration between VerifyTokenClerkAdapter and Clerk

--- src/infra/auth/clerk/verify-token/verify-token-clerk-adapter.spec.ts
+++ src/infra/auth/clerk/verify-token/verify-token-clerk-adapter.spec.ts
@@ -1,4 +1,5 @@
 import { VerifyTokenClerkAdapter } from './verify-token-clerk-adapter'
+import { Clerk } from '@clerk/clerk-sdk-node'
 
 jest.mock('@clerk/clerk-sdk-node', () => {
   return {
@@ -26,7 +27,27 @@ const makeSut = (): VerifyTokenClerkAdapter => {
 describe('VerifyTokenClerkAdapter', () => {
   it('Should return an user clerk id if Clerk verifyToken() is a success', async () => {
     const sut = makeSut()
-    const clerkUserId = await sut.execute('any_token')
-    expect(clerkUserId).toBe('user_2ZlNy6vFiPn9L286HdqSEQeEkLy')
+    const clerkResult = await sut.execute('any_token')
+    expect(clerkResult).toBe('user_2ZlNy6vFiPn9L286HdqSEQeEkLy')
+  })
+
+  it('Should return null if Clerk verifyToken() not returns payload', async () => {
+    const sut = makeSut()
+    const clerkMock = Clerk as jest.MockedFunction<any>
+    clerkMock.mockImplementation(() => ({
+      verifyToken: jest.fn(async () => { return {} })
+    }))
+    const clerkResult = await sut.execute('any_token')
+    expect(clerkResult).toBe(null)
+  })
+
+  it('Should throw if Clerk verifyToken() trows', async () => {
+    const sut = makeSut()
+    const clerkMock = Clerk as jest.MockedFunction<any>
+    clerkMock.mockImplementation(() => ({
+      verifyToken: jest.fn(async () => { throw new Error('any_message') })
+    }))
+    const promise = sut.execute('any_token')
+    await expect(promise).rejects.toThrow(new Error('any_message'))
   })
 })
