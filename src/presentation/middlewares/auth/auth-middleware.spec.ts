12c6f9b82dec226f2b45ce12533c17ea95acf926
mlucasdev <lucasmsantos819@gmail.com>
2023-12-19 14:56:14 -0300

    test: ensure integration between AuthMiddleware and Validation

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -1,14 +1,24 @@
-import { left, right } from '@/shared/either'
-import { AccessDeniedError, InvalidTokenError } from '@/domain/errors'
-import { type HttpRequest } from '@/presentation/types/http'
 import type { Auth, AuthResponse } from '@/domain/contracts/user'
-import { AuthMiddleware } from './auth-middleware'
-import { forbidden, ok, serverError, unauthorized } from '@/presentation/helpers/http-helpers'
+import { AccessDeniedError, InvalidTokenError } from '@/domain/errors'
+import { type Validation } from '@/presentation/contracts'
 import { AccessTokenNotInformedError } from '@/presentation/errors'
+import { forbidden, ok, serverError, unauthorized } from '@/presentation/helpers/http-helpers'
+import { type HttpRequest } from '@/presentation/types/http'
+import { left, right, type Either } from '@/shared/either'
+import { AuthMiddleware } from './auth-middleware'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: { accessToken: 'any_token' }
 })
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    async validate (input: any): Promise<Either<Error, null>> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new ValidationStub()
+}
 
 const makeAuth = (): Auth => {
   class AuthStub implements Auth {
@@ -21,13 +31,15 @@ const makeAuth = (): Auth => {
 
 interface SutTypes {
   sut: AuthMiddleware
+  validationStub: Validation
   authStub: Auth
 }
 
 const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
   const authStub = makeAuth()
-  const sut = new AuthMiddleware(authStub)
-  return { sut, authStub }
+  const sut = new AuthMiddleware(validationStub, authStub)
+  return { sut, validationStub, authStub }
 }
 
 describe('Auth Middleware', () => {
@@ -35,6 +47,33 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized(new AccessTokenNotInformedError()))
+  })
+
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().headers)
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    const error = new Error()
+    error.stack = 'any_stack'
+    expect(httpResponse).toEqual(serverError())
   })
 
   it('Should call Auth with correct values', async () => {
