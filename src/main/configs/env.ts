9986dc45e25eeef6f39aa94d69492d1b9d209a1c
mlucasdev <lucasmsantos819@gmail.com>
2023-12-20 18:38:24 -0300

    chore: add clerkJwtSecretKey

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -6,5 +6,5 @@ export default {
   webhookSecret: process.env.WEBHOOK_SECRET ?? 'any_webhook_secret',
   serverPort: process.env.SERVER_PORT ?? '5050',
   serverHost: process.env.SERVER_HOST ?? 'localhost',
-  clerkSecretKey: process.env.CLERK_SECRET_KEY ?? 'any_secret_key'
+  clerkJwtSecretKey: process.env.CLERK_JWT_SECRET_KEY ?? 'any_clerk_jwt_secret_key'
 }
