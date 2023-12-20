54cda42193b24c52f0cdcea749c0047d9c979c04
mlucasdev <lucasmsantos819@gmail.com>
2023-12-20 18:37:31 -0300

    feat: add JwtVerifyAdapter Factory

--- src/main/factories/infra/auth/clerk/verify-token-clerk-adapter-factory.ts
+++
@@ -1,6 +0,0 @@
-import { VerifyTokenClerkAdapter } from '@/infra/auth/clerk/verify-token/verify-token-clerk-adapter'
-import { type Decrypter } from '@/usecases/contracts/cryptography/decrypter'
-
-export const makeVerifyTokenClerkAdapterFactory = (): Decrypter => {
-  return new VerifyTokenClerkAdapter()
-}
