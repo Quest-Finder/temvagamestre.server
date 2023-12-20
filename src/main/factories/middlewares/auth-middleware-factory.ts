16f1574d793fe8274860d5d07b3609f3efe953cf
mlucasdev <lucasmsantos819@gmail.com>
2023-12-20 18:45:34 -0300

    refactor: remove auth validation

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -1,9 +1,7 @@
 import { type Middleware } from '@/presentation/contracts'
 import { AuthMiddleware } from '@/presentation/middlewares/auth/auth-middleware'
-import { AuthZodValidation } from '@/validators/user/auth/auh-zod-validation'
 import { makeAuthUseCase } from '../usecases/auth/auth-usecase-factory'
 
 export const makeAuthMiddleware = (): Middleware => {
-  const validation = new AuthZodValidation()
-  return new AuthMiddleware(validation, makeAuthUseCase())
+  return new AuthMiddleware(makeAuthUseCase())
 }
--- src/presentation/middlewares/auth/auth-middleware.ts
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -1,25 +1,18 @@
 import { type Auth } from '@/domain/contracts/user'
 import { InvalidTokenError } from '@/domain/errors'
-import type { Middleware, Validation } from '@/presentation/contracts'
+import type { Middleware } from '@/presentation/contracts'
 import { AccessTokenNotInformedError } from '@/presentation/errors'
 import { forbidden, ok, serverError, unauthorized } from '@/presentation/helpers/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/types/http'
 
 export class AuthMiddleware implements Middleware {
-  constructor (
-    private readonly validation: Validation,
-    private readonly auth: Auth
-  ) {}
+  constructor (private readonly auth: Auth) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (!accessToken) {
         return unauthorized(new AccessTokenNotInformedError())
-      }
-      const validationResult = await this.validation.validate(httpRequest.headers)
-      if (validationResult.isLeft()) {
-        return unauthorized(validationResult.value)
       }
       const authResult = await this.auth.perform(accessToken)
       if (authResult.isLeft()) {
