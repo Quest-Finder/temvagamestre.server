09bc66e65dae83a4e6dc868e4a31becd8c4b1407
mlucasdev <lucasmsantos819@gmail.com>
2023-12-20 18:29:22 -0300

    feat: create AuthNestMiddleware

---
+++ src/main/middlewares/auth/auth-nest-middleware.ts
@@ -0,0 +1,11 @@
+import { adaptMiddleware } from '@/main/factories/adapters'
+import { makeAuthMiddleware } from '@/main/factories/middlewares/auth-middleware-factory'
+import type { NestMiddleware } from '@nestjs/common'
+import type { NextFunction, Request, Response } from 'express'
+
+export class AuthNestMiddleware implements NestMiddleware {
+  async use (req: Request, res: Response, next: NextFunction): Promise<void> {
+    const adaptedMiddleware = adaptMiddleware(makeAuthMiddleware())
+    await adaptedMiddleware.adapt(req, res, next)
+  }
+}
