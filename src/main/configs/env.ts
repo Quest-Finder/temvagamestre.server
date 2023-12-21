c90eec10aab3047078992edd6057f83570f9cef3
mlucasdev <lucasmsantos819@gmail.com>
2023-12-21 14:44:34 -0300

    refactor: remove server host and rename server port to port

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -4,7 +4,6 @@ dotenv.config()
 
 export default {
   webhookSecret: process.env.WEBHOOK_SECRET ?? 'any_webhook_secret',
-  serverPort: process.env.SERVER_PORT ?? '5050',
-  serverHost: process.env.SERVER_HOST ?? 'localhost',
+  port: process.env.SERVER_PORT ?? '5050',
   clerkJwtSecretKey: process.env.CLERK_JWT_SECRET_KEY ?? 'any_clerk_jwt_secret_key'
 }
--- src/main/main.ts
+++ src/main/main.ts
@@ -6,8 +6,8 @@ async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     cors: true
   })
-  await app.listen(env.serverPort, env.serverHost)
-  console.log(`Server running at http://${env.serverHost}:${env.serverPort}`)
+  await app.listen(env.port)
+  console.log(`Server running at http://localhost:${env.port}`)
 }
 
 bootstrap()
