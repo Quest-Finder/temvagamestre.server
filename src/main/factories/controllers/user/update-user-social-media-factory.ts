645da3b153955214dc31410048cd2682050eec65
Natalia Perri <lnataliaperri@gmail.com>
2023-12-21 19:09:56 -0300

    refactor: updating factories name

--- src/main/factories/controllers/user/update-user-social-media-factory.ts
+++ src/main/factories/controllers/user/update-user-social-media-factory.ts
@@ -1,12 +1,12 @@
 import { type Controller } from '@/presentation/contracts'
 import { UpdateUserSocialMediaController } from '@/presentation/controllers/user/update-user-social-media/update-user-social-media-controller'
-import { makeUpdateUserSocialMediaUseCaseFactory } from '@/main/factories/usecases/user/update-user-social-media-usecase-factory'
+import { makeUpdateUserSocialMediaUseCase } from '@/main/factories/usecases/user/update-user-social-media-usecase-factory'
 import { UpdateUserSocialMediaValidation } from '@/validators/user/update-user-social-media-zod-validation'
 
-export const makeUpdateUserSocialMediaFactory = (): Controller => {
+export const makeUpdateUserSocialMedia = (): Controller => {
   const validation = new UpdateUserSocialMediaValidation()
   return new UpdateUserSocialMediaController(
     validation,
-    makeUpdateUserSocialMediaUseCaseFactory()
+    makeUpdateUserSocialMediaUseCase()
   )
 }
