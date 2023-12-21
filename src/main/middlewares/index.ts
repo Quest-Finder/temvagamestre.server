9e8e3d3c7f32897aea60dd3c0859ce9e6c5dd3ad
mlucasdev <lucasmsantos819@gmail.com>
2023-12-21 14:14:56 -0300

    refactor: move middlewares to middlewares folder

--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,2 +1,2 @@
-export * from './webhook-validator/webhook-validator-middleware'
-export * from './auth/auth-nest-middleware'
+export * from './webhook-validator-middleware'
+export * from './auth-nest-middleware'
