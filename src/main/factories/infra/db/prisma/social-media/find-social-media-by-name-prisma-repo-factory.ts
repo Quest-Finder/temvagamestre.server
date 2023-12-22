e76e0e69a73357aa340a7c83561facf331a2dd33
Natalia Perri <lnataliaperri@gmail.com>
2023-12-22 17:54:38 -0300

    feat: creating add social media usecases

---
+++ src/main/factories/infra/db/prisma/social-media/find-social-media-by-name-prisma-repo-factory.ts
@@ -0,0 +1,6 @@
+import { FindSocialMediaByNamePrismaRepo } from '@/infra/db/prisma/social-media/find-social-media-by-name/find-social-media-by-name-prisma-repo'
+import { type FindSocialMediaByNameRepo } from '@/usecases/contracts/db/social-media/find-social-media-by-name-repo'
+
+export const makeFindSocialMediaByNamePrismaRepo = (): FindSocialMediaByNameRepo => {
+  return new FindSocialMediaByNamePrismaRepo()
+}
