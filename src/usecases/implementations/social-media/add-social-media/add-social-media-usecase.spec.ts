e9a4713e79d4763d7febf4a8dce0d8d3c2bf7a06
Natalia Perri <lnataliaperri@gmail.com>
2023-12-22 17:37:38 -0300

    test: ensure AddSocialMediaUseCase calls FindSocialMediaByNameRepo with correct social media names

--- src/usecases/implementations/social-media/add-social-media/add-social-media-usecase.spec.ts
+++ src/usecases/implementations/social-media/add-social-media/add-social-media-usecase.spec.ts
@@ -1,3 +1,67 @@
+import { type AddSocialMediaRepo } from '@/usecases/contracts/db/social-media'
+import { AddSocialMediaUseCase } from './add-social-media-usecase'
+import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
+import { type FindSocialMediaByNameRepo } from '@/usecases/contracts/db/social-media/find-social-media-by-name-repo'
+import { type IdBuilder } from '@/usecases/contracts/id'
+import { SocialMedias } from '@/domain/contracts/social-media/social-medias'
+
+const makeSocialMediaModel = (): SocialMediaModel => ({
+  name: 'facebook',
+  id: 'any_social_media_id'
+})
+
+const makeIdBuilder = (): IdBuilder => {
+  class IdBuilderStub implements IdBuilder {
+    build (): string { return 'any_social_media_id' }
+  }
+  return new IdBuilderStub()
+}
+
+const makeFindSocialMediaByNameRepo = (): FindSocialMediaByNameRepo => {
+  class FindSocialMediaByNameRepoStub implements FindSocialMediaByNameRepo {
+    async execute (name: string): Promise<SocialMediaModel | null> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new FindSocialMediaByNameRepoStub()
+}
+
+const makeAddSocialMediaRepo = (): AddSocialMediaRepo => {
+  class AddSocialMediaRepoStub implements AddSocialMediaRepo {
+    async execute (data: SocialMediaModel): Promise<void> {
+      await Promise.resolve()
+    }
+  }
+  return new AddSocialMediaRepoStub()
+}
+
+type SutTypes = {
+  sut: AddSocialMediaUseCase
+  addSocialMediaRepo: AddSocialMediaRepo
+  findSocialMediaByNameRepo: FindSocialMediaByNameRepo
+  idBuilder: IdBuilder
+}
+
+const makeSut = (): SutTypes => {
+  const addSocialMediaRepo = makeAddSocialMediaRepo()
+  const findSocialMediaByNameRepo = makeFindSocialMediaByNameRepo()
+  const idBuilder = makeIdBuilder()
+  const sut = new AddSocialMediaUseCase(addSocialMediaRepo, findSocialMediaByNameRepo, idBuilder)
+
+  return {
+    sut,
+    addSocialMediaRepo,
+    findSocialMediaByNameRepo,
+    idBuilder
+  }
+}
+
 describe('AddSocialMediaUseCase', () => {
-  it('', async () => {})
+  it('Should call FindSocialMediaByNameRepo with correct social media name', async () => {
+    const { sut, findSocialMediaByNameRepo } = makeSut()
+    const executeSpy = jest.spyOn(findSocialMediaByNameRepo, 'execute')
+    await sut.perform()
+    expect(executeSpy).toHaveBeenCalledTimes(SocialMedias.getSocialMedias().length)
+    expect(executeSpy).toHaveBeenCalledWith(SocialMedias.getSocialMedias()[0])
+  })
 })
