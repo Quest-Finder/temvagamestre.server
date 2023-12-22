61edce82b76c8e64062ed949ce4350b8e4a85e3d
Natalia Perri <lnataliaperri@gmail.com>
2023-12-22 17:37:32 -0300

    feat: ensure AddSocialMediaUseCase calls FindSocialMediaByNameRepo with correct social media names

--- src/usecases/implementations/social-media/add-social-media/add-social-media-usecase.ts
+++ src/usecases/implementations/social-media/add-social-media/add-social-media-usecase.ts
@@ -1,16 +1,30 @@
-import { type AddSocialMedia, type AddSocialMediaData, type AddSocialMediaResponse } from '@/domain/contracts/social-media'
-import { type SocialMedias } from '@/domain/contracts/social-media/social-medias'
+import type { AddSocialMedia } from '@/domain/contracts/social-media'
+import { SocialMedias } from '@/domain/contracts/social-media/social-medias'
 import { type AddSocialMediaRepo } from '@/usecases/contracts/db/social-media'
+import { type FindSocialMediaByNameRepo } from '@/usecases/contracts/db/social-media/find-social-media-by-name-repo'
 import { type IdBuilder } from '@/usecases/contracts/id'
 
 export class AddSocialMediaUseCase implements AddSocialMedia {
   constructor (
-    private readonly socialMedias: SocialMedias,
     private readonly addSocialMediaRepo: AddSocialMediaRepo,
+    private readonly findSocialMediaByNameRepo: FindSocialMediaByNameRepo,
     private readonly idBuilder: IdBuilder
   ) {}
 
-  async perform (data: AddSocialMediaData): Promise<AddSocialMediaResponse> {
+  async perform (): Promise<void> {
+    const socialMedias = SocialMedias.getSocialMedias()
 
+    for (const socialMedia of socialMedias) {
+      const existingSocialMedia = await this.findSocialMediaByNameRepo.execute(socialMedia)
+
+      if (!existingSocialMedia) {
+        const id = this.idBuilder.build()
+
+        await this.addSocialMediaRepo.execute({
+          id,
+          name: socialMedia
+        })
+      }
+    }
   }
 }
