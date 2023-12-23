11de793a5c5e1cfa7050aac69fc68d47077f2300
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 12:12:20 -0300

    refactor: removing files

--- src/main/factories/infra/db/prisma/social-media/add-social-media-prisma-repo-factory.ts
+++
@@ -1,6 +0,0 @@
-import { AddSocialMediaPrismaRepo } from '@/infra/db/prisma/social-media/add-social-media/add-social-media-prisma-repo'
-import { type AddSocialMediaRepo } from '@/usecases/contracts/db/social-media'
-
-export const makeAddSocialMediaPrismaRepo = (): AddSocialMediaRepo => {
-  return new AddSocialMediaPrismaRepo()
-}
