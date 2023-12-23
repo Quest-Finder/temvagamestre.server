1bc51459f4bdbcc1c2fdfd4cf0f4c33ffe1c1664
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 12:56:52 -0300

    test: ensure FindManySocialMediasController return 200 on success

--- src/presentation/controllers/social-media/find-many-social-medias/find-many-social-medias-controller.spec.ts
+++ src/presentation/controllers/social-media/find-many-social-medias/find-many-social-medias-controller.spec.ts
@@ -2,7 +2,7 @@ import { type FindManySocialMedias, type FindManySocialMediasResponse } from '@/
 import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
 import { right } from '@/shared/either'
 import { FindManySocialMediasController } from './find-many-social-medias-controller'
-import { serverError } from '@/presentation/helpers/http-helpers'
+import { ok, serverError } from '@/presentation/helpers/http-helpers'
 
 const makeFakeSocialMediasModel = (): SocialMediaModel => ({
   id: 'any_social_media_id',
@@ -48,5 +48,11 @@ describe('FindManySocialMediasController', () => {
     jest.spyOn(findManySocialMediasStub, 'perform').mockReturnValueOnce(Promise.resolve(Promise.reject(new Error())))
     const httpResponse = await sut.handle()
     expect(httpResponse).toEqual(serverError())
+  })
+
+  it('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(ok([makeFakeSocialMediasModel()]))
   })
 })
