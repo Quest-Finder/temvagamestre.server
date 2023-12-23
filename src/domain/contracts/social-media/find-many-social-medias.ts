801649ffe342f91163337fbf258555f36b618031
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 12:12:15 -0300

    feat: ensure FindManySocialMediasRepo is called with no values

--- src/domain/contracts/social-media/find-many-social-medias.ts
+++ src/domain/contracts/social-media/find-many-social-medias.ts
@@ -1,5 +1,8 @@
 import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
+import { type Either } from '@/shared/either'
+
+export type FindManySocialMediasResponse = Either<Error, SocialMediaModel[] | []>
 
 export interface FindManySocialMedias {
-  perform: () => Promise<SocialMediaModel[] | []>
+  perform: () => Promise<FindManySocialMediasResponse>
 }
