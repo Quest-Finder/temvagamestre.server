6dea34888bedadd7d2051bb0a30873a3d000eb6d
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 12:54:32 -0300

    test: ensure FindManySocialMediasController returns 500 if FindManySocialMedias throws

--- src/presentation/controllers/social-media/find-many-social-medias/find-many-social-medias-controller.spec.ts
+++ src/presentation/controllers/social-media/find-many-social-medias/find-many-social-medias-controller.spec.ts
@@ -2,6 +2,7 @@ import { type FindManySocialMedias, type FindManySocialMediasResponse } from '@/
 import { type SocialMediaModel } from '@/domain/models/social-media/social-media-model'
 import { right } from '@/shared/either'
 import { FindManySocialMediasController } from './find-many-social-medias-controller'
+import { serverError } from '@/presentation/helpers/http-helpers'
 
 const makeFakeSocialMediasModel = (): SocialMediaModel => ({
   id: 'any_social_media_id',
@@ -40,5 +41,12 @@ describe('FindManySocialMediasController', () => {
 
     expect(performSpy).toHaveBeenCalledTimes(1)
     expect(performSpy).toHaveBeenCalledWith()
+  })
+
+  it('Should return 500 if FindManySocialMedias throws', async () => {
+    const { sut, findManySocialMediasStub } = makeSut()
+    jest.spyOn(findManySocialMediasStub, 'perform').mockReturnValueOnce(Promise.resolve(Promise.reject(new Error())))
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(serverError())
   })
 })
