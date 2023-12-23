522c04b3c967f02c5d61249f32a46e8fda263b9c
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 10:51:46 -0300

    refactor: removing unused errors

--- src/domain/errors/social-media-exists-error.ts
+++
@@ -1,6 +0,0 @@
-export class SocialMediaExistsError extends Error {
-  constructor (socialMediaName: string) {
-    super(`Social media ${socialMediaName} already exists.`)
-    this.name = 'SocialMediaExistsError'
-  }
-}
