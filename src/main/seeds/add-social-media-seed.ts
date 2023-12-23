c4aef8957545a8e9057326cbf848e7becfca2ebe
Natalia Perri <lnataliaperri@gmail.com>
2023-12-23 10:50:02 -0300

    refactor: adjusting seed function

--- src/main/seeds/add-social-media-seed.ts
+++ src/main/seeds/add-social-media-seed.ts
@@ -1,14 +1,11 @@
 import { makeAddSocialMediaUsecase } from '@/main/factories/usecases/social-media/add-social-media-usecase-factory'
 
 export const addSocialMediaSeed = (): void => {
-  const execute = async (): Promise<void> => {
-    try {
-      await makeAddSocialMediaUsecase().perform()
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    void makeAddSocialMediaUsecase().perform().then((() => { console.log('Success') }))
+  } catch (error) {
+    console.log(error)
   }
-  void execute().then((() => { console.log('Success') }))
 }
 
 addSocialMediaSeed()
