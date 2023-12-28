a480e6a33bbd277b8ff79167185eb61c2af35422
codedbylucas <lucasmsantos819@gmail.com>
2023-12-28 18:33:08 -0300

    test: ensure addSocialMediaSeed add all Social Medias

---
+++ src/main/seeds/add-social-media-seed.test.ts
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment ./src/main/configs/db-test/custom-environment-jest.ts
+ */
+
+import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
+import type { PrismaClient } from '@prisma/client'
+import { addSocialMediaSeed } from './add-social-media-seed'
+
+let prisma: PrismaClient
+
+describe('addSocialMediaSeed', () => {
+  beforeAll(async () => {
+    await PrismaHelper.connect()
+    prisma = await PrismaHelper.getPrisma()
+  })
+
+  beforeEach(async () => {
+    await prisma.socialMedia.deleteMany()
+  })
+
+  afterAll(async () => {
+    await PrismaHelper.disconnect()
+  })
+
+  it('Should add all Social Medias', async () => {
+    await addSocialMediaSeed()
+    const socialMedias = await prisma.socialMedia.findMany()
+    expect(socialMedias.length).toBe(4)
+  })
+})
