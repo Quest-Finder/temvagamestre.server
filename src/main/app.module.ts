45cefc7a38e5afa728016f9d0df95ebc47edae8f
codedbylucas <lucasmsantos819@gmail.com>
2023-12-28 18:35:10 -0300

    refactor: move SignUp Route to routes folder

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { SocialMediaModule } from './routes/social-media/social-media-route.module'
-import { SignUpModule } from './routes/user/signup/signup.module'
+import { SignUpModule } from './routes/signup/signup.module'
 import { UserModule } from './routes/user/user-route.module'
 
 @Module({
