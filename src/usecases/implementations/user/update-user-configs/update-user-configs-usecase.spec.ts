2bee50fab324686b56a80b544c5ad6478121aedf
codedbylucas <lucasmsantos819@gmail.com>
2023-12-30 16:31:21 -0300

    test: ensure UpdateUserConfigsUseCase return PhoneNotCreatedError if user phone does not exist

--- src/usecases/implementations/user/update-user-configs/update-user-configs-usecase.spec.ts
+++ src/usecases/implementations/user/update-user-configs/update-user-configs-usecase.spec.ts
@@ -1,6 +1,7 @@
 import { type UserModel } from '@/domain/models'
 import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
 import { UpdateUserConfigsUseCase } from './update-user-configs-usecase'
+import { PhoneNotCreatedError } from '@/domain/errors'
 
 describe('UpdateUserConfigsUseCase', () => {
   it('Should call FindUserByIdRepo with correct user id', async () => {
@@ -24,5 +25,25 @@ describe('UpdateUserConfigsUseCase', () => {
     })
     expect(executeSpy).toHaveBeenCalledWith('any_user_id')
     expect(executeSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return PhoneNotCreatedError if user phone does not exist', async () => {
+    class FindUserByIdRepoStub implements FindUserByIdRepo {
+      async execute (id: string): Promise<UserModel | null> {
+        return {
+          id: 'any_user_id',
+          email: 'any_email@mail.com',
+          firstName: 'any_first_name',
+          lastName: 'any_last_name'
+        }
+      }
+    }
+    const findUserByIdRepoStub = new FindUserByIdRepoStub()
+    const sut = new UpdateUserConfigsUseCase(findUserByIdRepoStub)
+    const result = await sut.perform({
+      userId: 'any_user_id',
+      allowMessage: true
+    })
+    expect(result.value).toEqual(new PhoneNotCreatedError())
   })
 })
