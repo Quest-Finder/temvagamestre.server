b26cdd858abd98c8c7d2224e775f1860717512a7
codedbylucas <lucasmsantos819@gmail.com>
2023-12-30 16:30:59 -0300

    feat: ensure UpdateUserConfigsUseCase return PhoneNotCreatedError if user phone does not exist

--- src/usecases/implementations/user/update-user-configs/update-user-configs-usecase.ts
+++ src/usecases/implementations/user/update-user-configs/update-user-configs-usecase.ts
@@ -1,12 +1,17 @@
 import type { UpdateUserConfigs, UpdateUserConfigsData, UpdateUserConfigsResponse } from '@/domain/contracts/user'
-import { right } from '@/shared/either'
+import { PhoneNotCreatedError } from '@/domain/errors'
+import { type UserModel } from '@/domain/models'
+import { left, right } from '@/shared/either'
 import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
 
 export class UpdateUserConfigsUseCase implements UpdateUserConfigs {
   constructor (private readonly findUserByIdRepo: FindUserByIdRepo) {}
 
   async perform (data: UpdateUserConfigsData): Promise<UpdateUserConfigsResponse> {
-    await this.findUserByIdRepo.execute(data.userId)
+    const user = await this.findUserByIdRepo.execute(data.userId) as UserModel
+    if (!user.phone) {
+      return left(new PhoneNotCreatedError())
+    }
     return right(null)
   }
 }
