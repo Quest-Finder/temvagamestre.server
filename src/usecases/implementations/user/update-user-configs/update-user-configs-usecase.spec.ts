09af88aa853f13f49f3b12127535c88cedf00857
codedbylucas <lucasmsantos819@gmail.com>
2023-12-30 16:22:56 -0300

    test: ensure UpdateUserConfigsUseCase call FindUserByIdRepo with correct user id

---
+++ src/usecases/implementations/user/update-user-configs/update-user-configs-usecase.spec.ts
@@ -0,0 +1,28 @@
+import { type UserModel } from '@/domain/models'
+import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
+import { UpdateUserConfigsUseCase } from './update-user-configs-usecase'
+
+describe('UpdateUserConfigsUseCase', () => {
+  it('Should call FindUserByIdRepo with correct user id', async () => {
+    class FindUserByIdRepoStub implements FindUserByIdRepo {
+      async execute (id: string): Promise<UserModel | null> {
+        return {
+          id: 'any_user_id',
+          email: 'any_email@mail.com',
+          firstName: 'any_first_name',
+          lastName: 'any_last_name',
+          phone: 'any_phone'
+        }
+      }
+    }
+    const findUserByIdRepoStub = new FindUserByIdRepoStub()
+    const executeSpy = jest.spyOn(findUserByIdRepoStub, 'execute')
+    const sut = new UpdateUserConfigsUseCase(findUserByIdRepoStub)
+    await sut.perform({
+      userId: 'any_user_id',
+      allowMessage: true
+    })
+    expect(executeSpy).toHaveBeenCalledWith('any_user_id')
+    expect(executeSpy).toHaveBeenCalledTimes(1)
+  })
+})
