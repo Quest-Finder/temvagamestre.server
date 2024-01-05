1a9ec524578107a07db704b23758df388746f273
codedbylucas <lucasmsantos819@gmail.com>
2024-01-05 16:21:55 -0300

    feat: add UpdateUserZodValidation

---
+++ src/validators/user/update-user-zod-validation.ts
@@ -0,0 +1,17 @@
+import type { Validation } from '@/presentation/contracts'
+import type { Either } from '@/shared/either'
+import { ZodHelper } from '@/validators/helpers/zod-helper'
+import { z } from 'zod'
+
+export class UpdateUserZodValidation implements Validation {
+  async validate (input: any): Promise<Either<Error, null>> {
+    const schema = z.object({
+      firstName: z.string().min(2).max(25).optional(),
+      lastName: z.string().min(2).max(50).optional(),
+      nickname: z.string().min(5).max(25).optional(),
+      phone: z.string().min(11).max(20).optional(),
+      dateOfBirth: z.string().min(10).max(10).optional()
+    })
+    return ZodHelper.check({ value: input, schema })
+  }
+}
