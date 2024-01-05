cb53ae3740f9fcfdcc6d62c5510a35163f1f31d7
codedbylucas <lucasmsantos819@gmail.com>
2024-01-05 14:19:38 -0300

    feat: ensure UpdateUserUseCase not call formatDateStringToDateTime() if dateOfBirth not provided

--- src/usecases/implementations/user/update-user/udpate-user-usecase.ts
+++ src/usecases/implementations/user/update-user/udpate-user-usecase.ts
@@ -3,6 +3,8 @@ import { formatDateStringToDateTime } from '@/util'
 
 export class UpdateUserUseCase implements UpdateUser {
   async perform (data: UpdateUserData): Promise<void> {
-    formatDateStringToDateTime(data.dateOfBirth as string)
+    if (data.dateOfBirth) {
+      formatDateStringToDateTime(data.dateOfBirth)
+    }
   }
 }
