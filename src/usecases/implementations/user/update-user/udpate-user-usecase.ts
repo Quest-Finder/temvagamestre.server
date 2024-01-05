6fbcb79b281d51d2cb4b891fc6f4ed4c75dac99c
codedbylucas <lucasmsantos819@gmail.com>
2024-01-05 14:58:40 -0300

    feat: ensure integration between UpdateUserUseCase and UpdateUserRepo

--- src/usecases/implementations/user/update-user/udpate-user-usecase.ts
+++ src/usecases/implementations/user/update-user/udpate-user-usecase.ts
@@ -1,10 +1,16 @@
 import type { UpdateUserData, UpdateUser } from '@/domain/contracts/user'
+import type { UpdateUserRepoData, UpdateUserRepo } from '@/usecases/contracts/db/user'
 import { formatDateStringToDateTime } from '@/util'
 
 export class UpdateUserUseCase implements UpdateUser {
+  constructor (private readonly updateUserRepo: UpdateUserRepo) {}
+
   async perform (data: UpdateUserData): Promise<void> {
-    if (data.dateOfBirth) {
-      formatDateStringToDateTime(data.dateOfBirth)
+    const { dateOfBirth, ...dataToRepo } = data
+    const dataRepo: UpdateUserRepoData = dataToRepo
+    if (dateOfBirth) {
+      dataRepo.dateOfBirth = formatDateStringToDateTime(dateOfBirth)
     }
+    await this.updateUserRepo.execute(dataRepo)
   }
 }
