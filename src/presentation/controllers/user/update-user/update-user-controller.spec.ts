06a6c0ba4b5674b1f09c5820425c38a61253f9d7
codedbylucas <lucasmsantos819@gmail.com>
2024-01-05 15:06:10 -0300

    test: ensure integration between UpdateUserController and Validation

---
+++ src/presentation/controllers/user/update-user/update-user-controller.spec.ts
@@ -0,0 +1,64 @@
+import type { Validation } from '@/presentation/contracts'
+import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
+import type { HttpRequest } from '@/presentation/types/http'
+import { left, right, type Either } from '@/shared/either'
+import { UpdateUserController } from './update-user-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    firstName: 'any_first_name',
+    lastName: 'any_last_name',
+    phone: 'any_phone',
+    dateOfBirth: '12-31-2000',
+    nickname: 'any_nickname'
+  }
+})
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    async validate (input: any): Promise<Either<Error, null>> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new ValidationStub()
+}
+
+type SutTypes = {
+  sut: UpdateUserController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new UpdateUserController(validationStub)
+  return { sut, validationStub }
+}
+
+describe('UpdateUserController', () => {
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    const error = new Error()
+    error.stack = 'any_stack'
+    expect(httpResponse).toEqual(serverError())
+  })
+})
