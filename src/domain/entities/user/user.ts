44992818ec9a40e38f28a10643b71335b3d33041
codedbylucas <lucasmsantos819@gmail.com>
2024-01-08 08:39:06 -0300

    feat: ensure User return return valid result on success

--- src/domain/entities/user/user.ts
+++ src/domain/entities/user/user.ts
@@ -1,4 +1,4 @@
-import { left, type Either } from '@/shared/either'
+import { left, type Either, right } from '@/shared/either'
 import type { InvalidDateOfBirthError, InvalidFirstNameError, InvalidLastNameError, InvalidPhoneError } from './errors'
 import { DateOfBirth, FirstName, LastName, Phone } from './value-objects'
 
@@ -11,27 +11,38 @@ export type UpdateUserEntityData = {
 
 type UserErrors = InvalidDateOfBirthError | InvalidFirstNameError | InvalidLastNameError | InvalidPhoneError
 
-export type UserResponse = Either<UserErrors, User>
+type UpdateUserValueObjects = {
+  firstName?: FirstName
+  lastName?: LastName
+  phone?: Phone
+  dateOfBirth?: DateOfBirth
+}
+
+export type UpdateUserResponse = Either<UserErrors, UpdateUserValueObjects>
+
+type ValueObjectsResults = {
+  firstName?: Either<InvalidFirstNameError, FirstName>
+  lastName?: Either<InvalidLastNameError, LastName>
+  phone?: Either<InvalidPhoneError, Phone>
+  dateOfBirth?: Either<InvalidDateOfBirthError, DateOfBirth>
+}
 
 export class User {
-  private constructor (
-    private readonly firstName: FirstName,
-    private readonly lastName: LastName,
-    private readonly phone: Phone,
-    private readonly dateOfBirth: DateOfBirth
-  ) {
-    Object.freeze(this)
-  }
-
-  static update (data: UpdateUserEntityData): UserResponse {
-    const results: Array<Either<UserErrors, FirstName | LastName | Phone | DateOfBirth>> = []
-    if (data.firstName) results.push(FirstName.create(data.firstName))
-    if (data.lastName) results.push(LastName.create(data.lastName))
-    if (data.phone) results.push(Phone.create(data.phone))
-    if (data.dateOfBirth) results.push(DateOfBirth.create(data.dateOfBirth))
-    for (const result of results) {
+  static update (data: UpdateUserEntityData): UpdateUserResponse {
+    const results: ValueObjectsResults = {
+      ...(data.firstName && { firstName: FirstName.create(data.firstName) }),
+      ...(data.lastName && { lastName: LastName.create(data.lastName) }),
+      ...(data.phone && { phone: Phone.create(data.phone) }),
+      ...(data.dateOfBirth && { dateOfBirth: DateOfBirth.create(data.dateOfBirth) })
+    }
+    for (const result of Object.values(results)) {
       if (result.isLeft()) return left(result.value)
     }
-    return left(new Error())
+    return right({
+      ...(results.firstName && { firstName: results.firstName.value as FirstName }),
+      ...(results.lastName && { lastName: results.lastName.value as LastName }),
+      ...(results.phone && { phone: results.phone.value as Phone }),
+      ...(results.dateOfBirth && { dateOfBirth: results.dateOfBirth.value as DateOfBirth })
+    })
   }
 }
