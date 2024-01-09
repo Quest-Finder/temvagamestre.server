be4e3710b8e2e43c001a832b6d6dfd066d335f91
Natalia Perri <lnataliaperri@gmail.com>
2024-01-09 15:06:01 -0300

    test: ensure UpdateUserPreferenceUsecase calls FindPreferenceByIdRepo with correct values

---
+++ src/usecases/implementations/user/update-user-preference/update-user-preference-usecase.spec.ts
@@ -0,0 +1,60 @@
+import { type FindPreferenceByIdRepo, type UpdateUserPreferenceRepo } from '@/usecases/contracts/db/user'
+import { UpdateUserPreferenceUseCase } from './update-user-preference-usecase'
+import { type UpdateUserPreferenceData } from '@/domain/contracts/user'
+import { type PreferenceModel } from '@/domain/models'
+
+const makeFakePreferenceModel = (): PreferenceModel => ({
+  id: 'any_user_id',
+  frequency: 'daily',
+  activeType: 'gameMaster'
+})
+
+const makeFakeUpdateUserPreferenceData = (): UpdateUserPreferenceData => ({
+  id: 'any_user_id',
+  frequency: 'daily'
+})
+
+const makeFindPreferenceByIdRepo = (): FindPreferenceByIdRepo => {
+  class FindPreferenceByIdRepoStub implements FindPreferenceByIdRepo {
+    async execute (id: string): Promise<PreferenceModel | null> {
+      return await Promise.resolve(makeFakePreferenceModel())
+    }
+  }
+
+  return new FindPreferenceByIdRepoStub()
+}
+
+const makeUpdateUserPreferenceRepo = (): UpdateUserPreferenceRepo => {
+  class UpdateUserPreferenceRepoStub implements UpdateUserPreferenceRepo {
+    async execute (data: UpdateUserPreferenceData): Promise<void> {
+      await Promise.resolve()
+    }
+  }
+  return new UpdateUserPreferenceRepoStub()
+}
+
+type SutTypes = {
+  sut: UpdateUserPreferenceUseCase
+  updateUserPreferenceRepoStub: UpdateUserPreferenceRepo
+  findPreferenceByIdRepoStub: FindPreferenceByIdRepo
+}
+
+const makeSut = (): SutTypes => {
+  const findPreferenceByIdRepoStub = makeFindPreferenceByIdRepo()
+  const updateUserPreferenceRepoStub = makeUpdateUserPreferenceRepo()
+  const sut = new UpdateUserPreferenceUseCase(findPreferenceByIdRepoStub, updateUserPreferenceRepoStub)
+  return {
+    sut,
+    updateUserPreferenceRepoStub,
+    findPreferenceByIdRepoStub
+  }
+}
+
+describe('UpdateUserPreferenceUsecase', () => {
+  it('Should call FindPreferenceByIdRepo with correct values', async () => {
+    const { sut, findPreferenceByIdRepoStub } = makeSut()
+    const executeSpy = jest.spyOn(findPreferenceByIdRepoStub, 'execute')
+    await sut.perform(makeFakeUpdateUserPreferenceData())
+    expect(executeSpy).toHaveBeenCalledWith('any_user_id')
+  })
+})
