438159957bcfeff029a57d21a2440ce4af8e29c5
Natalia Perri <lnataliaperri@gmail.com>
2024-01-09 08:03:47 -0300

    test: ensure FindPreferenceByIdPrismaRepo returns a preference if prisma findUnique() is a success

---
+++ src/infra/db/prisma/user/user-preferences-exists-by-user-id/find-preference-by-id-prisma-repo.spec.ts
@@ -0,0 +1,52 @@
+import { FindPreferenceIdPrismaRepo } from './find-preference-by-id-prisma-repo'
+import { ActiveType, Frequency, type Preference, type PrismaClient } from '@prisma/client'
+import { PrismockClient } from 'prismock'
+import { PrismaHelper } from '../../helpers/prisma-helper'
+import { type UserModel } from '@/domain/models'
+
+let prismock: PrismaClient
+
+const makeFakeUserModel = (): UserModel => ({
+  id: 'any_user_id',
+  lastName: 'any_last_name',
+  firstName: 'any_first_name',
+  nickname: 'any_nick_name',
+  phone: 'any_user_phone',
+  dateOfBirth: new Date(),
+  addressId: 'any_address_id',
+  email: 'any_email'
+})
+
+const makeFakePreference = (): Preference => ({
+  id: 'any_user_id',
+  frequency: Frequency.daily,
+  activeType: ActiveType.player
+})
+
+const makeSut = (): FindPreferenceIdPrismaRepo => {
+  return new FindPreferenceIdPrismaRepo()
+}
+
+describe('FindPreferenceByIdPrismaRepo', () => {
+  beforeEach(async () => {
+    await prismock.preference.deleteMany()
+    await prismock.user.deleteMany()
+  })
+
+  beforeAll(async () => {
+    prismock = new PrismockClient()
+    jest.spyOn(PrismaHelper, 'getPrisma').mockReturnValue(Promise.resolve(prismock))
+  })
+
+  afterAll(async () => {
+    await prismock.$disconnect()
+  })
+
+  it('Should return a Preference if prisma findUnique() is a success', async () => {
+    const sut = makeSut()
+    await prismock.user.create({ data: makeFakeUserModel() })
+    await prismock.preference.create({ data: makeFakePreference() })
+    const preference = await sut.execute('any_user_id')
+    expect(preference).toEqual({ ...makeFakePreference() })
+  })
+})
