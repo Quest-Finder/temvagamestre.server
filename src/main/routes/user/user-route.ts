9435fc4f1a9fdcb320c44730217005ff357a700b
Natalia Perri <lnataliaperri@gmail.com>
2024-01-10 16:26:55 -0300

    feat: ensure POST /user/preference returns 204 on success

--- src/main/routes/user/user-route.ts
+++ src/main/routes/user/user-route.ts
@@ -1,4 +1,5 @@
 import { adaptRoute } from '@/main/factories/adapters'
+import { makeAddUserPreferenceController } from '@/main/factories/controllers/user/add-user-preference-controller-factory'
 import { makeUpdateUserPreferenceController } from '@/main/factories/controllers/user/update-user-preference-controller-factory'
 import { makeUpdateUserSocialMediaController } from '@/main/factories/controllers/user/update-user-social-media-controller-factory'
 import { Controller, Post, Put, Req, Res } from '@nestjs/common'
@@ -10,6 +11,14 @@ export class UserRoute {
   async updateSocialMedia (@Req() req: Request, @Res() res: Response): Promise<void> {
     const adaptNest = adaptRoute(
       makeUpdateUserSocialMediaController()
+    )
+    await adaptNest.adapt(req, res)
+  }
+
+  @Post('/preference')
+  async addUserPreference (@Req() req: Request, @Res() res: Response): Promise<void> {
+    const adaptNest = adaptRoute(
+      makeAddUserPreferenceController()
     )
     await adaptNest.adapt(req, res)
   }
