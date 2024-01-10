9d283412186b4b8c588f32117cde31f8b3c99d62
Natalia Perri <lnataliaperri@gmail.com>
2024-01-10 15:18:43 -0300

    test: ensure AddUserPreferenceController returns 500 if Validation throws

--- src/presentation/controllers/user/add-user-preference/add-user-preference-controller.spec.ts
+++ src/presentation/controllers/user/add-user-preference/add-user-preference-controller.spec.ts
@@ -3,7 +3,7 @@ import { type Validation } from '@/presentation/contracts'
 import { type HttpRequest } from '@/presentation/types/http'
 import { right, type Either, left } from '@/shared/either'
 import { AddUserPreferenceController } from './add-user-preference-controller'
-import { badRequest } from '@/presentation/helpers/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: {
@@ -65,5 +65,14 @@ describe('AddUserPreferenceController', () => {
     )
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError())
   })
 })
