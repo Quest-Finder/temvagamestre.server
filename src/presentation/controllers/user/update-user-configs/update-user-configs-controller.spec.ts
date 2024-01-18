2a64d7f993d7a0bd741490af2679fe90896fca81
codedbylucas <lucasmsantos819@gmail.com>
2024-01-18 19:39:59 -0300

    test: ensure UpdateUserConfigsController call UpdateUserConfigs with correct values

--- src/presentation/controllers/user/update-user-configs/update-user-configs-controller.spec.ts
+++ src/presentation/controllers/user/update-user-configs/update-user-configs-controller.spec.ts
@@ -1,9 +1,39 @@
+import { type UpdateUserConfigs, type UpdateUserConfigsData, type UpdateUserConfigsResponse } from '@/domain/contracts/user'
 import { UpdateUserConfigsController } from './update-user-configs-controller'
+import { right } from '@/shared/either'
 
 describe('UpdateUserConfigsController', () => {
   it('Should return 400 if allow message was not provided', async () => {
-    const sut = new UpdateUserConfigsController()
-    const httpResponse = await sut.handle({ body: {} })
+    class UpdateUserConfigsStub implements UpdateUserConfigs {
+      async perform (data: UpdateUserConfigsData): Promise<UpdateUserConfigsResponse> {
+        return await Promise.resolve(right(null))
+      }
+    }
+    const updateUserConfigsStub = new UpdateUserConfigsStub()
+    const sut = new UpdateUserConfigsController(updateUserConfigsStub)
+    const httpResponse = await sut.handle({
+      headers: { userId: 'any_user_id' },
+      body: {}
+    })
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should call UpdateUserConfigs with correct values', async () => {
+    class UpdateUserConfigsStub implements UpdateUserConfigs {
+      async perform (data: UpdateUserConfigsData): Promise<UpdateUserConfigsResponse> {
+        return await Promise.resolve(right(null))
+      }
+    }
+    const updateUserConfigsStub = new UpdateUserConfigsStub()
+    const sut = new UpdateUserConfigsController(updateUserConfigsStub)
+    const performSpy = jest.spyOn(updateUserConfigsStub, 'perform')
+    await sut.handle({
+      headers: { userId: 'any_user_id' },
+      body: { allowMessage: true }
+    })
+    expect(performSpy).toHaveBeenCalledWith({
+      userId: 'any_user_id',
+      allowMessage: true
+    })
   })
 })
