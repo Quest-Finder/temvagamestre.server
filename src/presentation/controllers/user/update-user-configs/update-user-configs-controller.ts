e5e2327ea2fe4f71f1fd0ea0ac112707d69280e3
codedbylucas <lucasmsantos819@gmail.com>
2024-01-18 19:39:11 -0300

    feat: ensure UpdateUserConfigsController call UpdateUserConfigs with correct values

--- src/presentation/controllers/user/update-user-configs/update-user-configs-controller.ts
+++ src/presentation/controllers/user/update-user-configs/update-user-configs-controller.ts
@@ -1,8 +1,15 @@
+import { type UpdateUserConfigs } from '@/domain/contracts/user'
 import { type Controller } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/types/http'
 
 export class UpdateUserConfigsController implements Controller {
+  constructor (private readonly updateUserConfigs: UpdateUserConfigs) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.updateUserConfigs.perform({
+      userId: httpRequest.headers.userId,
+      allowMessage: httpRequest.body.allowMessage
+    })
     return { body: '', statusCode: 400 }
   }
 }
