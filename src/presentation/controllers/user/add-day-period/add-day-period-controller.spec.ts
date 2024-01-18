4593aa503fec6f32794844f965295f6eec8629a9
Natalia Perri <lnataliaperri@gmail.com>
2024-01-18 19:43:01 -0300

    test: ensure AddDayPeriodController returns 500 if Validation throws

--- src/presentation/controllers/user/add-day-period/add-day-period-controller.spec.ts
+++ src/presentation/controllers/user/add-day-period/add-day-period-controller.spec.ts
@@ -3,7 +3,7 @@ import { type Validation } from '@/presentation/contracts'
 import { type HttpRequest } from '@/presentation/types/http'
 import { type Either, right, left } from '@/shared/either'
 import { AddDayPeriodController } from './add-day-period-controller'
-import { badRequest } from '@/presentation/helpers/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: {
@@ -66,5 +66,14 @@ describe('AddDayPeriodController', () => {
     )
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError())
   })
 })
