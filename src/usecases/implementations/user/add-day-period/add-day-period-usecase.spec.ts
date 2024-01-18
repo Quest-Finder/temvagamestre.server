7a16704f7363d0f6e5d324dcb5b7082db11b9bcd
Natalia Perri <lnataliaperri@gmail.com>
2024-01-18 19:32:21 -0300

    test: ensure AddDayPeriodUsecase calls FindPreferenceByIdRepo with correct values

---
+++ src/usecases/implementations/user/add-day-period/add-day-period-usecase.spec.ts
@@ -0,0 +1,64 @@
+import { type AddDayPeriodData } from '@/domain/contracts/user'
+import { type PreferenceModel } from '@/domain/models'
+import { type DayPeriodModel } from '@/domain/models/day-period/day-period-model'
+import { type AddOrUpdateDayPeriodRepo, type FindPreferenceByIdRepo } from '@/usecases/contracts/db/user'
+import { AddDayPeriodUsecase } from './add-day-period-usecase'
+
+const makeFakeDayPeriodModel = (): DayPeriodModel => ({
+  id: 'any_user_id',
+  morning: true,
+  afternoon: false,
+  night: false
+})
+
+const makeFakePreferenceModel = (): PreferenceModel => ({
+  id: 'any_user_id',
+  frequency: 'daily',
+  activeType: 'gameMaster'
+})
+
+const makeFindPreferenceByIdRepo = (): FindPreferenceByIdRepo => {
+  class FindPreferenceByIdRepoStub implements FindPreferenceByIdRepo {
+    async execute (id: string): Promise<PreferenceModel | null> {
+      return await Promise.resolve(makeFakePreferenceModel())
+    }
+  }
+
+  return new FindPreferenceByIdRepoStub()
+}
+
+const makeAddOrUpdateDayPeriodRepo = (): AddOrUpdateDayPeriodRepo => {
+  class AddOrUpdateDayPeriodRepoStub implements AddOrUpdateDayPeriodRepo {
+    async execute (data: AddDayPeriodData): Promise<void> {
+      await Promise.resolve()
+    }
+  }
+  return new AddOrUpdateDayPeriodRepoStub()
+}
+
+type SutTypes = {
+  sut: AddDayPeriodUsecase
+  findPreferenceByIdRepoStub: FindPreferenceByIdRepo
+  addOrUpdateDayPeriodRepoStub: AddOrUpdateDayPeriodRepo
+}
+
+const makeSut = (): SutTypes => {
+  const findPreferenceByIdRepoStub = makeFindPreferenceByIdRepo()
+  const addOrUpdateDayPeriodRepoStub = makeAddOrUpdateDayPeriodRepo()
+  const sut = new AddDayPeriodUsecase(findPreferenceByIdRepoStub, addOrUpdateDayPeriodRepoStub)
+
+  return {
+    findPreferenceByIdRepoStub,
+    addOrUpdateDayPeriodRepoStub,
+    sut
+  }
+}
+
+describe('AddDayPeriodUsecase', () => {
+  it('Should call FindPreferenceByIdRepo with correct values', async () => {
+    const { sut, findPreferenceByIdRepoStub } = makeSut()
+    const executeSpy = jest.spyOn(findPreferenceByIdRepoStub, 'execute')
+    await sut.perform(makeFakeDayPeriodModel())
+    expect(executeSpy).toHaveBeenCalledWith('any_user_id')
+  })
+})
