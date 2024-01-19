85dece3d1ac05160e78d21609dda5693a86594ab
codedbylucas <lucasmsantos819@gmail.com>
2024-01-18 21:05:32 -0300

    test: ensure UpdateUserConfigsController return 500 if Validation throws

--- src/presentation/controllers/user/update-user-configs/update-user-configs-controller.spec.ts
+++ src/presentation/controllers/user/update-user-configs/update-user-configs-controller.spec.ts
@@ -3,7 +3,7 @@ import { UpdateUserConfigsController } from './update-user-configs-controller'
 import { type Either, left, right } from '@/shared/either'
 import { type Validation } from '@/presentation/contracts'
 import { type HttpRequest } from '@/presentation/types/http'
-import { badRequest, noContent } from '@/presentation/helpers/http-helpers'
+import { badRequest, noContent, serverError } from '@/presentation/helpers/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: { userId: 'any_user_id' },
@@ -56,6 +56,15 @@ describe('UpdateUserConfigsController', () => {
     )
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError())
   })
 
   it('Should call UpdateUserConfigs with correct values', async () => {
