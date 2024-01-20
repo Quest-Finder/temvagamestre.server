aa59cb6bf9c210b6da3275a39c75ebd64989d4f4
Natalia Perri <lnataliaperri@gmail.com>
2024-01-20 16:21:11 -0300

    test: ensure AddGamePlaceController calls Validation with correct values

---
+++ src/presentation/controllers/user/add-game-place/add-game-place-controller.spec.ts
@@ -0,0 +1,59 @@
+import { type AddGamePlace, type AddGamePlaceData, type AddGamePlaceResponse } from '@/domain/contracts/user'
+import { type Validation } from '@/presentation/contracts'
+import { type HttpRequest } from '@/presentation/types/http'
+import { type Either, right } from '@/shared/either'
+import { AddGamePlaceController } from './add-game-place-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    userId: 'any_user_id'
+  },
+  body: {
+    online: true,
+    inPerson: false
+  }
+})
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    async validate (input: any): Promise<Either<Error, null>> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeFakeAddGamePlace = (): AddGamePlace => {
+  class AddGamePlaceStub implements AddGamePlace {
+    async perform (data: AddGamePlaceData): Promise<AddGamePlaceResponse> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new AddGamePlaceStub()
+}
+
+type SutTypes = {
+  sut: AddGamePlaceController
+  validationStub: Validation
+  addGamePlaceStub: AddGamePlace
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const addGamePlaceStub = makeFakeAddGamePlace()
+  const sut = new AddGamePlaceController(validationStub, addGamePlaceStub)
+  return {
+    sut,
+    addGamePlaceStub,
+    validationStub
+  }
+}
+
+describe('AddGamePlaceController', () => {
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+})
