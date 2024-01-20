5197b76016f66e5e27e7b446816c6e5836faf9d0
Natalia Perri <lnataliaperri@gmail.com>
2024-01-20 16:21:37 -0300

    test: ensure AddGamePlaceController returns 400 if  Validation fails

--- src/presentation/controllers/user/add-game-place/add-game-place-controller.spec.ts
+++ src/presentation/controllers/user/add-game-place/add-game-place-controller.spec.ts
@@ -1,8 +1,9 @@
 import { type AddGamePlace, type AddGamePlaceData, type AddGamePlaceResponse } from '@/domain/contracts/user'
 import { type Validation } from '@/presentation/contracts'
 import { type HttpRequest } from '@/presentation/types/http'
-import { type Either, right } from '@/shared/either'
+import { type Either, right, left } from '@/shared/either'
 import { AddGamePlaceController } from './add-game-place-controller'
+import { badRequest } from '@/presentation/helpers/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: {
@@ -55,5 +56,14 @@ describe('AddGamePlaceController', () => {
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
   })
 })
