e11b389eec4a7285b896d886188f3966d136cba5
Natalia Perri <lnataliaperri@gmail.com>
2024-01-20 16:31:53 -0300

    feat: ensure POST /user/preference/game-place returns 204 on success

--- src/main/factories/controllers/user/add-game-place-controller-factory.ts
+++ src/main/factories/controllers/user/add-game-place-controller-factory.ts
@@ -1,7 +1,7 @@
 import { AddGamePlaceController } from '@/presentation/controllers/user/add-game-place/add-game-place-controller'
 import { AddGamePlaceZodValidation } from '@/validators/user/add-game-place/add-game-place-zod-validation'
-import { type Controller } from '@nestjs/common/interfaces'
 import { makeAddGamePlaceUsecase } from '../../usecases/user/add-game-place-usecase-factory'
+import { type Controller } from '@/presentation/contracts'
 
 export const makeAddGamePlaceController = (): Controller => {
   const validation = new AddGamePlaceZodValidation()
--- src/main/routes/user/user-route.ts
+++ src/main/routes/user/user-route.ts
@@ -5,6 +5,7 @@ import { makeUpdateUserSocialMediaController } from '@/main/factories/controller
 import { makeUpdateUserController } from '@/main/factories/controllers/user/update-user-controller-factory'
 import { Controller, Patch, Post, Req, Res, Put } from '@nestjs/common'
 import { Request, Response } from 'express'
+import { makeAddGamePlaceController } from '@/main/factories/controllers/user/add-game-place-controller-factory'
 
 @Controller('/user')
 export class UserRoute {
@@ -35,6 +36,14 @@ export class UserRoute {
   @Patch()
   async updateUser (@Req() req: Request, @Res() res: Response): Promise<void> {
     const adaptNest = adaptRoute(makeUpdateUserController())
+    await adaptNest.adapt(req, res)
+  }
+
+  @Post('/preference/game-place')
+  async addGamePlace (@Req() req: Request, @Res() res: Response): Promise<void> {
+    const adaptNest = adaptRoute(
+      makeAddGamePlaceController()
+    )
     await adaptNest.adapt(req, res)
   }
 }
