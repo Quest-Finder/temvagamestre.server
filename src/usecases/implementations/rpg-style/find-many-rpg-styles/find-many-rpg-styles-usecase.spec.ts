7b6f512c9649ee0fc69f723d214820ebf8b5aa06
Natalia Perri <lnataliaperri@gmail.com>
2024-01-26 19:34:45 -0300

    test: ensure FindManyRpgStylesUsecase calls FindManyRpgStylesRepo with no values

---
+++ src/usecases/implementations/rpg-style/find-many-rpg-styles/find-many-rpg-styles-usecase.spec.ts
@@ -0,0 +1,42 @@
+import { type RpgStyleModel } from '@/domain/models'
+import { type FindManyRpgStylesRepo } from '@/usecases/contracts/db/rpg-style'
+import { FindManyRpgStylesUsecase } from './find-many-rpg-styles-usecase'
+
+const makeFakeRpgStyleModel = (): RpgStyleModel => ({
+  id: 'any_rpg_style_id',
+  name: 'any_rpg_style_name'
+})
+
+const makeFindManyRpgStylesRepo = (): FindManyRpgStylesRepo => {
+  class FindManyRpgStylesRepoStub implements FindManyRpgStylesRepo {
+    async execute (): Promise<RpgStyleModel[] | []> {
+      return await Promise.resolve([makeFakeRpgStyleModel()])
+    }
+  }
+  return new FindManyRpgStylesRepoStub()
+}
+
+type SutTypes = {
+  sut: FindManyRpgStylesUsecase
+  findManyRpgStylesRepoStub: FindManyRpgStylesRepo
+}
+
+const makeSut = (): SutTypes => {
+  const findManyRpgStylesRepoStub = makeFindManyRpgStylesRepo()
+  const sut = new FindManyRpgStylesUsecase(findManyRpgStylesRepoStub)
+
+  return {
+    findManyRpgStylesRepoStub,
+    sut
+  }
+}
+
+describe('FindManyRpgStylesUsecase', () => {
+  it('Should call FindManyRpgStylesRepo with no values', async () => {
+    const { sut, findManyRpgStylesRepoStub } = makeSut()
+    const executeSpy = jest.spyOn(findManyRpgStylesRepoStub, 'execute')
+    await sut.perform()
+    expect(executeSpy).toHaveBeenCalledTimes(1)
+    expect(executeSpy).toHaveBeenCalledWith()
+  })
+})
