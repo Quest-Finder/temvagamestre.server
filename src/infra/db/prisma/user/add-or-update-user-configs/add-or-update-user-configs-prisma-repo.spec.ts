adb8b5e6974697ecddd964d87cd756410a28a81a
codedbylucas <lucasmsantos819@gmail.com>
2024-01-26 17:26:53 -0300

    test: ensure AddOrUpdateUserConfigsPrismaRepo add an User if Prisma create() is a success

---
+++ src/infra/db/prisma/user/add-or-update-user-configs/add-or-update-user-configs-prisma-repo.spec.ts
@@ -0,0 +1,38 @@
+import { type PrismaClient } from '@prisma/client'
+import { AddOrUpdateUserConfigsPrismaRepo } from './add-or-update-user-configs-prisma-repo'
+import { PrismockClient } from 'prismock'
+import { PrismaHelper } from '../../helpers/prisma-helper'
+import { type UserModel, type UserConfigModel } from '@/domain/models'
+
+const makeFakeUserConfigModel = (): UserConfigModel => ({
+  id: 'any_user_id',
+  allowMessage: true
+})
+
+const makeFakeUserModel = (): UserModel => ({
+  id: 'any_user_id',
+  lastName: 'any_last_name',
+  firstName: 'any_first_name',
+  email: 'any_email@mail.com'
+})
+
+let prismock: PrismaClient
+
+describe('AddOrUpdateUserConfigsPrismaRepo', () => {
+  beforeAll(async () => {
+    prismock = new PrismockClient()
+    jest.spyOn(PrismaHelper, 'getPrisma').mockReturnValue(
+      Promise.resolve(prismock)
+    )
+  })
+
+  it('Should add an User if Prisma create() is a success', async () => {
+    const sut = new AddOrUpdateUserConfigsPrismaRepo()
+    await prismock.user.create({ data: makeFakeUserModel() })
+    await sut.execute(makeFakeUserConfigModel())
+    const userConfig = await prismock.userConfig.findUnique({
+      where: { id: 'any_user_id' }
+    })
+    expect(userConfig).toEqual(makeFakeUserConfigModel())
+  })
+})
