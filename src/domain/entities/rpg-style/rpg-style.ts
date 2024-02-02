cce0e8d83670a8b9fd16e0584251fab3bfbf51ab
codedbylucas <lucasmsantos819@gmail.com>
2024-02-02 18:01:08 -0300

    refactor: move RpgStyle to entity folder

--- src/domain/entities/rpg-style/rpg-style.ts
+++ src/domain/entities/rpg-style/rpg-style.ts
@@ -1,7 +1,7 @@
-export class RpgStyles {
+export class RpgStyle {
   private static readonly rpgStyleNames: string [] = ['MMORPGs']
 
   static getRpgStyles (): string[] {
-    return [...this.rpgStyleNames]
+    return this.rpgStyleNames
   }
 }
