8e31ba631af50954a326d68682280506390d0a1d
codedbylucas <lucasmsantos819@gmail.com>
2024-02-02 18:08:37 -0300

    feat: ensure FindManyRpgStylesUsecase return all rpg styles on success

--- src/usecases/contracts/db/rpg-style/find-many-rpg-styles-repo.ts
+++ src/usecases/contracts/db/rpg-style/find-many-rpg-styles-repo.ts
@@ -1,5 +1,5 @@
 import { type RpgStyleModel } from '@/domain/models'
 
 export interface FindManyRpgStylesRepo {
-  execute: () => Promise<RpgStyleModel[] | []>
+  execute: () => Promise<RpgStyleModel[]>
 }
--- src/usecases/implementations/rpg-style/find-many-rpg-styles/find-many-rpg-styles-usecase.ts
+++ src/usecases/implementations/rpg-style/find-many-rpg-styles/find-many-rpg-styles-usecase.ts
@@ -3,13 +3,10 @@ import { right } from '@/shared/either'
 import { type FindManyRpgStylesRepo } from '@/usecases/contracts/db/rpg-style'
 
 export class FindManyRpgStylesUsecase implements FindManyRpgStyles {
-  constructor (
-    private readonly findManyRpgStylesRepo: FindManyRpgStylesRepo
-  ) {}
+  constructor (private readonly findManyRpgStylesRepo: FindManyRpgStylesRepo) {}
 
   async perform (): Promise<FindManyRpgStylesResponse> {
     const rpgStyles = await this.findManyRpgStylesRepo.execute()
-
     return right(rpgStyles)
   }
 }
