c5f2f2a36d66ef8c0252013194f187610066a851
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 10:13:29 -0300

    refactor: move AddDayPeriodUsecase to day-period folder

--- src/main/factories/usecases/user/add-day-period-usecase-factory.ts
+++ src/main/factories/usecases/user/add-day-period-usecase-factory.ts
@@ -1,5 +1,5 @@
 import { type AddDayPeriod } from '@/domain/contracts/user'
-import { AddDayPeriodUsecase } from '@/usecases/implementations/user/add-day-period/add-day-period-usecase'
+import { AddDayPeriodUsecase } from '@/usecases/implementations/day-period/add-day-period/add-day-period-usecase'
 import { makeFindPreferenceByIdPrismaRepo } from '../../infra/db/prisma/user/find-preference-by-id-prisma-repo-factory'
 import { makeAddOrUpdateDayPeriodPrismaRepo } from '../../infra/db/prisma/user/add-or-update-day-period-prisma-repo-factory'
 
