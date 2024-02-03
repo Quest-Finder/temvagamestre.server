da3fa905dc28fd04481d1a31237c70beab327672
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 10:23:06 -0300

    refactor: move AddGamePlaceUsecase to game-place folder

--- src/main/factories/usecases/user/add-game-place-usecase-factory.ts
+++ src/main/factories/usecases/user/add-game-place-usecase-factory.ts
@@ -1,5 +1,5 @@
 import { type AddGamePlace } from '@/domain/contracts/user'
-import { AddGamePlaceUsecase } from '@/usecases/implementations/user/add-game-place/add-game-place-usecase'
+import { AddGamePlaceUsecase } from '@/usecases/implementations/game-place/add-game-place/add-game-place-usecase'
 import { makeFindPreferenceByIdPrismaRepo } from '../../infra/db/prisma/user/find-preference-by-id-prisma-repo-factory'
 import { makeAddOrUpdateGamePlacePrismaRepo } from '../../infra/db/prisma/user/add-or-update-game-place-prisma-repo-factory'
 
