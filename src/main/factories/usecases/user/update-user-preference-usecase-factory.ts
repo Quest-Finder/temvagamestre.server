ba759d50e65901b3f030c001397f66f79aae39f9
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 10:17:06 -0300

    refactor: move UpdateUserPreferenceUseCase to preference folder

--- src/main/factories/usecases/user/update-user-preference-usecase-factory.ts
+++ src/main/factories/usecases/user/update-user-preference-usecase-factory.ts
@@ -1,5 +1,5 @@
 import { type UpdateUserPreference } from '@/domain/contracts/user'
-import { UpdateUserPreferenceUseCase } from '@/usecases/implementations/user/update-user-preference/update-user-preference-usecase'
+import { UpdateUserPreferenceUseCase } from '@/usecases/implementations/preference/update-user-preference/update-user-preference-usecase'
 import { makeFindPreferenceByIdPrismaRepo } from '../../infra/db/prisma/user/find-preference-by-id-prisma-repo-factory'
 import { makeUpdateUserPreferenceRepo } from '../../infra/db/prisma/user/update-user-preference-repo-factory'
 
