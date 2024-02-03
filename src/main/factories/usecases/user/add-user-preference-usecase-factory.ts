bd119466cb0ef6cc4a6692c0bbc5e863f51b5d1b
codedbylucas <lucasmsantos819@gmail.com>
2024-02-03 10:12:34 -0300

    refactor: move AddUserPreferenceUsecase to preference folder

--- src/main/factories/usecases/user/add-user-preference-usecase-factory.ts
+++ src/main/factories/usecases/user/add-user-preference-usecase-factory.ts
@@ -1,6 +1,6 @@
 import { type AddUserPreference } from '@/domain/contracts/user/add-user-preference'
 import { makeFindPreferenceByIdPrismaRepo } from '../../infra/db/prisma/user/find-preference-by-id-prisma-repo-factory'
-import { AddUserPreferenceUsecase } from '@/usecases/implementations/user/add-user-preference/add-user-preference-usecase'
+import { AddUserPreferenceUsecase } from '@/usecases/implementations/preference/add-user-preference/add-user-preference-usecase'
 import { makeAddUserPreferencePrismaRepo } from '../../infra/db/prisma/user/add-user-preference-prisma-repo-factory'
 
 export const makeAddUserPreferenceUseCase = (): AddUserPreference => {
---
+++ src/usecases/implementations/preference/add-user-preference/add-user-preference-usecase.ts
@@ -0,0 +1,24 @@
+import type { AddUserPreference, AddUserPreferenceData, AddUserPreferenceResponse } from '@/domain/contracts/user/add-user-preference'
+import { ExistentUserPreferenceError } from '@/domain/errors'
+import { left, right } from '@/shared/either'
+import type { AddUserPreferenceRepo, FindPreferenceByIdRepo } from '@/usecases/contracts/db/user'
+
+export class AddUserPreferenceUsecase implements AddUserPreference {
+  constructor (
+    private readonly findPreferenceByIdRepo: FindPreferenceByIdRepo,
+    private readonly addUserPreferenceRepo: AddUserPreferenceRepo
+  ) {}
+
+  async perform (data: AddUserPreferenceData): Promise<AddUserPreferenceResponse> {
+    const { id } = data
+    const userPreferenceExists = await this.findPreferenceByIdRepo.execute(id)
+
+    if (userPreferenceExists) {
+      return left(new ExistentUserPreferenceError(id))
+    }
+
+    await this.addUserPreferenceRepo.execute(data)
+
+    return right(null)
+  }
+}
