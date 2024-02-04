85a599d771f92194691967562a3181f2a5a9759d
codedbylucas <lucasmsantos819@gmail.com>
2024-02-04 17:21:29 -0300

    refactor: move classes that talk to UserPreference table to the user-preference folder

--- src/main/factories/infra/db/prisma/user/update-user-preference-repo-factory.ts
+++ src/main/factories/infra/db/prisma/user/update-user-preference-repo-factory.ts
@@ -1,4 +1,4 @@
-import { UpdateUserPreferencePrismaRepo } from '@/infra/db/prisma/data/user/update-user-preferences/update-user-preference-prisma-repo'
+import { UpdateUserPreferencePrismaRepo } from '@/infra/db/prisma/data/user-preference/update-user-preference/update-user-preference-prisma-repo'
 import { type UpdateUserPreferenceRepo } from '@/usecases/contracts/db/user'
 
 export const makeUpdateUserPreferenceRepo = (): UpdateUserPreferenceRepo => {
