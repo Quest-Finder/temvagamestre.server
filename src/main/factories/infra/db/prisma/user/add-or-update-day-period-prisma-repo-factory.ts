95cacc86e125e6b13f3bb028fe89a89f3e0fba9b
codedbylucas <lucasmsantos819@gmail.com>
2024-02-04 17:25:11 -0300

    refactor: move AddOrUpdateDayPeriodPrismaRepo to user-preference folder

--- src/main/factories/infra/db/prisma/user/add-or-update-day-period-prisma-repo-factory.ts
+++ src/main/factories/infra/db/prisma/user/add-or-update-day-period-prisma-repo-factory.ts
@@ -1,4 +1,4 @@
-import { AddOrUpdateDayPeriodPrismaRepo } from '@/infra/db/prisma/data/user/add-or-update-day-period/add-or-update-day-period-prisma-repo'
+import { AddOrUpdateDayPeriodPrismaRepo } from '@/infra/db/prisma/data/user-preference-day-period/add-or-update-day-period/add-or-update-day-period-prisma-repo'
 import { type AddOrUpdateDayPeriodRepo } from '@/usecases/contracts/db/user'
 
 export const makeAddOrUpdateDayPeriodPrismaRepo = (): AddOrUpdateDayPeriodRepo => {
