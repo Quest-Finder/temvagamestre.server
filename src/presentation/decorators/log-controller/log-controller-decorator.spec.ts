154a90b83a25d9d6a440ba3c2121fdaaad7017f2
codedbylucas <lucasmsantos819@gmail.com>
2024-02-06 15:16:28 -0300

    test: ensure LogControllerDecorator call Controller with http request

---
+++ src/presentation/decorators/log-controller/log-controller-decorator.spec.ts
@@ -0,0 +1,47 @@
+import type { Controller } from '@/presentation/contracts'
+import { ok } from '@/presentation/helpers/http-helpers'
+import type { HttpResponse, HttpRequest } from '@/presentation/types/http'
+import { LogControllerDecorator } from './log-controller-decorator'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any name',
+    email: 'any_email',
+    password: 'password1234',
+    passwordConfirmation: 'password1234'
+  }
+})
+
+const makeFakeData = (): { value: string, data: string } => ({
+  value: 'any_value',
+  data: 'any_data'
+})
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return { sut, controllerStub }
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return await Promise.resolve(ok(makeFakeData()))
+    }
+  }
+  return new ControllerStub()
+}
+
+describe('LogControllerDecorator', () => {
+  it('Should call Controller with http request', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
