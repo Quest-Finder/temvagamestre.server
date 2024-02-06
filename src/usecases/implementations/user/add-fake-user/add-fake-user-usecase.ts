4bd0a9241ec80ee96444b38593ab38d74a39ebce
codedbylucas <lucasmsantos819@gmail.com>
2024-02-06 18:27:53 -0300

    feat: ensure AddFakeUserUseCase call AddUser with correct values

--- src/usecases/implementations/user/add-fake-user/add-fake-user-usecase.ts
+++ src/usecases/implementations/user/add-fake-user/add-fake-user-usecase.ts
@@ -1,11 +1,21 @@
-import type { AddFakeUser, AddFakeUserResponse } from '@/domain/contracts/user'
+import type { AddFakeUser, AddFakeUserResponse, AddUser } from '@/domain/contracts/user'
 import { type IdBuilder } from '@/usecases/contracts/id'
 
 export class AddFakeUserUseCase implements AddFakeUser {
-  constructor (private readonly idBuilder: IdBuilder) {}
+  constructor (
+    private readonly idBuilder: IdBuilder,
+    private readonly addUser: AddUser
+  ) {}
 
   async perform (): Promise<AddFakeUserResponse> {
-    this.idBuilder.build()
+    const id = this.idBuilder.build()
+    const value = Math.random().toString().substring(2, 8)
+    await this.addUser.perform({
+      externalAuthUserId: id,
+      email: `email_${value}@mail.com`,
+      lastName: `last_name_${value}`,
+      firstName: `first_name_${value}`
+    })
     return { token: '' }
   }
 }
