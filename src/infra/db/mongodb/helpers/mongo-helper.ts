785b81a5bd82ecab6af41ae661ecba332bf7a87c
codedbylucas <lucasmsantos819@gmail.com>
2024-02-06 16:27:45 -0300

    feat: ensure MongoHelper reconnect if MongoDB is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,26 @@
+import { type Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient | null,
+  uri: null as unknown as string,
+
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(this.uri)
+    console.log(`MongoDB running at ${this.uri}`)
+  },
+
+  async disconnect (): Promise<void> {
+    if (this.client) {
+      await this.client.close()
+      this.client = null
+    }
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.db) {
+      this.client = await MongoClient.connect(this.uri)
+    }
+    return this.client.db().collection(name)
+  }
+}
--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -5,5 +5,6 @@ dotenv.config()
 export default {
   webhookSecret: process.env.WEBHOOK_SECRET ?? 'any_webhook_secret',
   port: process.env.PORT ?? '5050',
-  clerkJwtSecretKey: process.env.CLERK_JWT_SECRET_KEY ?? 'any_clerk_jwt_secret_key'
+  clerkJwtSecretKey: process.env.CLERK_JWT_SECRET_KEY ?? 'any_clerk_jwt_secret_key',
+  mongoDbUri: process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/rpg-log'
 }
