98aba831d5eeb8ee30770b2cc179e754d7823925
codedbylucas <lucasmsantos819@gmail.com>
2024-02-06 19:02:31 -0300

    test: ensure integration between JwtSignAdapter and jsonwebtoken

---
+++ src/infra/cryptography/jwt-adapter/sign/jwt-sign-adapter.spec.ts
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import { JwtSignAdapter } from './jwt-sign-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'any_token'
+  }
+}))
+
+const makeSut = (): JwtSignAdapter => {
+  return new JwtSignAdapter('any_secret')
+}
+
+describe('JwtSignAdapter', () => {
+  it('Should call sign with correct values', async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt, 'sign')
+    sut.execute('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'any_secret', { expiresIn: '1h' })
+  })
+
+  it('Should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    expect(() => sut.execute('any_id')).toThrow()
+  })
+
+  it('Should return a token if sign success', async () => {
+    const sut = makeSut()
+    const accessToken = sut.execute('any_id')
+    expect(accessToken).toEqual({ token: 'any_token' })
+  })
+})
