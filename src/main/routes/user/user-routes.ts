b84100070af68795c4368cf94e7c5db6daae2017
codedbylucas <lucasmsantos819@gmail.com>
2024-02-07 16:58:09 -0300

    docs: add @ApiOperation to UserRoutes

--- src/main/routes/user/user-routes.ts
+++ src/main/routes/user/user-routes.ts
@@ -1,13 +1,17 @@
 import { adaptRoute } from '@/main/factories/adapters'
 import { makeUpdateUserController } from '@/main/factories/controllers/user/update-user-controller-factory'
 import { Controller, Patch, Req, Res } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiTags, ApiOperation } from '@nestjs/swagger'
 import { Request, Response } from 'express'
 
 @ApiTags('User')
 @Controller('/user')
 export class UserRoutes {
   @Patch()
+  @ApiOperation({
+    summary: 'Atualiza um usuário',
+    description: 'Atualiza informações referente ao próprio usuário logado'
+  })
   async updateUser (@Req() req: Request, @Res() res: Response): Promise<void> {
     const adaptNest = adaptRoute(makeUpdateUserController())
     await adaptNest.adapt(req, res)
