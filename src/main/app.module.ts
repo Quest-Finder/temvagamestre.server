7d7a796c1f99dbb789a5c0e9c9d52f536a1eca27
codedbylucas <lucasmsantos819@gmail.com>
2024-02-07 15:09:24 -0300

    refactor: move UserPreferenceDayPeriodRoutes to UserPreferenceRoutes Module

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { RpgStyleRoutesModule } from './routes/rpg-style/rpg-style-routes.module'
 import { SocialMediaRoutesModule } from './routes/social-media/social-media-routes.module'
-import { UserPreferenceDayPeriodRoutesModule } from './routes/user-preference-day-period/user-preference-day-period-routes.module'
 import { UserPreferenceRoutesModule } from './routes/user-preference/user-preference-routes.module'
 import { UserSocialMediaRoutesModule } from './routes/user-social-media/user-social-media-routes.module'
 import { UserRoutesModule } from './routes/user/user-routes.module'
@@ -13,7 +12,6 @@ import { UserPreferenceGamePlaceRoutesModule } from './routes/user-preference-ga
     SocialMediaRoutesModule,
     UserSocialMediaRoutesModule,
     UserPreferenceRoutesModule,
-    UserPreferenceDayPeriodRoutesModule,
     UserPreferenceGamePlaceRoutesModule,
     RpgStyleRoutesModule
   ]
--- src/main/routes/user-preference/user-preference-routes.module.ts
+++ src/main/routes/user-preference/user-preference-routes.module.ts
@@ -1,9 +1,13 @@
 import { type MiddlewareConsumer, Module, type NestModule, RequestMethod } from '@nestjs/common'
 import { UserPreferenceRoutes } from './user-preference-routes'
 import { AuthNestMiddleware } from '@/main/middlewares'
+import { UserPreferenceDayPeriodRoutes } from './user-preference-day-period/user-preference-day-period-routes'
 
 @Module({
-  controllers: [UserPreferenceRoutes]
+  controllers: [
+    UserPreferenceRoutes,
+    UserPreferenceDayPeriodRoutes
+  ]
 })
 export class UserPreferenceRoutesModule implements NestModule {
   configure (consumer: MiddlewareConsumer): void {
@@ -11,7 +15,8 @@ export class UserPreferenceRoutesModule implements NestModule {
       .apply(AuthNestMiddleware)
       .forRoutes(
         { path: '/user/preference', method: RequestMethod.POST },
-        { path: '/user/preference', method: RequestMethod.PATCH }
+        { path: '/user/preference', method: RequestMethod.PATCH },
+        { path: '/user/preference/day-period', method: RequestMethod.POST }
       )
   }
 }
