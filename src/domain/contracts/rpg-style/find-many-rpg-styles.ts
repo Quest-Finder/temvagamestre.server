a56127d63717d6406310490ee4c5d6a9ddff05e7
codedbylucas <lucasmsantos819@gmail.com>
2024-02-13 10:22:39 -0300

    refactor: change FindManyRpgStyles return to an RpgStyleModel array

--- src/domain/contracts/rpg-style/find-many-rpg-styles.ts
+++ src/domain/contracts/rpg-style/find-many-rpg-styles.ts
@@ -1,8 +1,5 @@
 import { type RpgStyleModel } from '@/domain/models'
-import { type Either } from '@/shared/either'
-
-export type FindManyRpgStylesResponse = Either<Error, RpgStyleModel[]>
 
 export interface FindManyRpgStyles {
-  perform: () => Promise<FindManyRpgStylesResponse>
+  perform: () => Promise<RpgStyleModel[]>
 }
--- src/presentation/controllers/rpg-style/find-many-rpg-styles/find-many-rpg-styles-controller.spec.ts
+++ src/presentation/controllers/rpg-style/find-many-rpg-styles/find-many-rpg-styles-controller.spec.ts
@@ -1,18 +1,20 @@
-import type { FindManyRpgStyles, FindManyRpgStylesResponse } from '@/domain/contracts/rpg-style'
+import type { FindManyRpgStyles } from '@/domain/contracts/rpg-style'
 import { type RpgStyleModel } from '@/domain/models'
-import { right } from '@/shared/either'
-import { FindManyRpgStylesController } from './find-many-rpg-styles-controller'
 import { ok, serverError } from '@/presentation/helpers/http-helpers'
+import { FindManyRpgStylesController } from './find-many-rpg-styles-controller'
 
-const makeFakeRpgStyleModel = (): RpgStyleModel => ({
+const makeFakeRpgStyleModel = (): RpgStyleModel[] => ([{
   id: 'any_rpg_style_id',
   name: 'any_rpg_style_name'
-})
+}, {
+  id: 'other_rpg_style_id',
+  name: 'other_rpg_style_name'
+}])
 
 const makeFindManyRpgStyles = (): FindManyRpgStyles => {
   class FindManyRpgStylesStub implements FindManyRpgStyles {
-    async perform (): Promise<FindManyRpgStylesResponse> {
-      return await Promise.resolve(right([makeFakeRpgStyleModel()]))
+    async perform (): Promise<RpgStyleModel[]> {
+      return await Promise.resolve(makeFakeRpgStyleModel())
     }
   }
   return new FindManyRpgStylesStub()
@@ -40,7 +42,9 @@ describe('FindManyRpgStylesController', () => {
 
   it('Should return 500 if FindManyRpgStyles throws', async () => {
     const { sut, findManyRpgStylesStub } = makeSut()
-    jest.spyOn(findManyRpgStylesStub, 'perform').mockReturnValueOnce(Promise.resolve(Promise.reject(new Error())))
+    jest.spyOn(findManyRpgStylesStub, 'perform').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
     const httpResponse = await sut.handle()
     expect(httpResponse).toEqual(serverError(new Error()))
   })
@@ -48,6 +52,6 @@ describe('FindManyRpgStylesController', () => {
   it('Should return 200 on success', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle()
-    expect(httpResponse).toEqual(ok([makeFakeRpgStyleModel()]))
+    expect(httpResponse).toEqual(ok(makeFakeRpgStyleModel()))
   })
 })
