711f34389d3ecf6e25d797fd6a4462d4a6e2fdab
codedbylucas <lucasmsantos819@gmail.com>
2024-02-13 10:12:25 -0300

    feat: add overload to right function

--- src/shared/either.ts
+++ src/shared/either.ts
@@ -3,31 +3,25 @@ export type Either<L extends Error, R> = Left<L, R> | Right<L, R>
 class Left<L extends Error, R> {
   constructor (readonly value: L) {}
 
-  isLeft (): this is Left<L, R> {
-    return true
-  }
+  isLeft (): this is Left<L, R> { return true }
 
-  isRight (): this is Right<L, R> {
-    return false
-  }
+  isRight (): this is Right<L, R> { return false }
 }
 
 class Right<L extends Error, R> {
   constructor (readonly value: R) {}
 
-  isLeft (): this is Left<L, R> {
-    return false
-  }
+  isLeft (): this is Left<L, R> { return false }
 
-  isRight (): this is Right<L, R> {
-    return true
-  }
+  isRight (): this is Right<L, R> { return true }
 }
 
-export const left = <L extends Error, R>(l: L): Either<L, R> => {
-  return new Left<L, R>(l)
+export const left = <L extends Error, R>(error: L): Left<L, R> => {
+  return new Left<L, R>(error)
 }
 
-export const right = <L extends Error, R>(r: R): Either<L, R> => {
-  return new Right<L, R>(r)
+export function right <L extends Error, R extends void> (result?: R): Right<L, R>
+export function right <L extends Error, R> (result: R): Right<L, R>
+export function right <L extends Error, R> (result: R): Right<L, R> {
+  return new Right<L, R>(result)
 }
