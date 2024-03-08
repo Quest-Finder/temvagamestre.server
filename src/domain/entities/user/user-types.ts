e10e40fba2da4049a8a620aac30f8b528e9b72b3
codedbylucas <lucasmsantos819@gmail.com>
2024-03-08 20:13:50 -0300

    feat: ensure a User is successfully registered

--- src/domain/entities/user/user-types.ts
+++ src/domain/entities/user/user-types.ts
@@ -1,24 +1,19 @@
 import { type Either } from '@/shared/either'
-import type { InvalidDateOfBirthError, InvalidFirstNameError, InvalidLastNameError, InvalidPhoneError } from './errors'
-import type { DateOfBirth, FirstName, LastName, Phone } from './value-objects'
+import type { InvalidDateOfBirthError, InvalidNameError, InvalidPronounError, InvalidUsernameError } from './errors'
+import { type User } from './user'
+import { type PronounEnum } from './value-objects'
 
-export type UpdateUserEntityData = {
-  firstName?: string
-  lastName?: string
-  phone?: string
-  dateOfBirth?: string
-  userName?: string
+export type RegisterUserData = {
+  name: string
+  dateOfBirth: string
+  username: string
+  pronoun: PronounEnum
 }
 
-export type UserEntityErrors = InvalidDateOfBirthError | InvalidFirstNameError | InvalidLastNameError | InvalidPhoneError
+export type UserEntityErrors =
+  InvalidDateOfBirthError |
+  InvalidUsernameError |
+  InvalidNameError |
+  InvalidPronounError
 
-type UpdateUserValueResponse = UpdateUserEntityData
-
-export type UpdateUserEntityResponse = Either<UserEntityErrors, UpdateUserValueResponse>
-
-export type UpdateUserEntityValueObjectsResults = {
-  firstName?: Either<InvalidFirstNameError, FirstName>
-  lastName?: Either<InvalidLastNameError, LastName>
-  phone?: Either<InvalidPhoneError, Phone>
-  dateOfBirth?: Either<InvalidDateOfBirthError, DateOfBirth>
-}
+export type UpdateUserResponse = Either<UserEntityErrors, User>
