db401fcfe4e5ee41a271cf8887c006703f7bc08c
codedbylucas <lucasmsantos819@gmail.com>
2024-03-09 09:24:37 -0300

    feat: ensure integration between RegisterUserUseCase and RegisterUserRepo

--- src/usecases/implementations/user/register-user/register-user-usecase.ts
+++ src/usecases/implementations/user/register-user/register-user-usecase.ts
@@ -1,14 +1,17 @@
 import type { RegisterUser, UpdateUserResponse } from '@/domain/contracts/user'
 import { User, type RegisterUserData } from '@/domain/entities/user'
 import { left, right } from '@/shared/either'
+import { type RegisterUserRepo } from '@/usecases/contracts/db/user'
 
 export class RegisterUserUseCase implements RegisterUser {
+  constructor (private readonly registerUserRepo: RegisterUserRepo) {}
+
   async perform (data: RegisterUserData): Promise<UpdateUserResponse> {
     const registerUserResult = User.register(data)
     if (registerUserResult.isLeft()) {
       return left(registerUserResult.value)
     }
-
+    await this.registerUserRepo.execute(registerUserResult.value)
     return right()
   }
 }
