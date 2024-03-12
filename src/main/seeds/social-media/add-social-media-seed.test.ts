6864962f35a4069948bc7c54a5dd9d392e5a41de
codedbylucas <lucasmsantos819@gmail.com>
2024-03-12 20:00:49 -0300

    test: ensure addSocialMediaSeed add all social media

--- src/main/seeds/social-media/add-social-media-seed.test.ts
+++ src/main/seeds/social-media/add-social-media-seed.test.ts
@@ -21,6 +21,28 @@ describe('addSocialMediaSeed', () => {
   it('Should add all Social Medias', async () => {
     await addSocialMediaSeed
     const socialMedias = await prisma.socialMedia.findMany()
-    expect(socialMedias.length).toBe(4)
+    expect(socialMedias.length).toBe(5)
+    expect(socialMedias).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        name: 'Instagram',
+        baseUri: 'instagram.com/'
+      }),
+      expect.objectContaining({
+        name: 'Discord',
+        baseUri: 'discordapp.com/users/'
+      }),
+      expect.objectContaining({
+        name: 'X',
+        baseUri: 'x.com/'
+      }),
+      expect.objectContaining({
+        name: 'Reddit',
+        baseUri: 'reddit.com/user/'
+      }),
+      expect.objectContaining({
+        name: 'Facebook',
+        baseUri: 'facebook.com/'
+      })
+    ]))
   })
 })
