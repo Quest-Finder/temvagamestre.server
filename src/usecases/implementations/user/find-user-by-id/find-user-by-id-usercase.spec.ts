2dc67510cae4bfb047385896e6481de12738d91a
Felipe Souza <felipedb91@gmail.com>
2024-03-29 20:36:22 -0300

    test: ensure findUserByRepo is called with correct values

--- src/usecases/implementations/user/find-user-by-id/find-user-by-id-usercase.spec.ts
+++ src/usecases/implementations/user/find-user-by-id/find-user-by-id-usercase.spec.ts
@@ -1,15 +1,28 @@
 import { type FindUserById } from '@/domain/contracts/user'
+import { type UserModel } from '@/domain/models'
+import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
 import { FindUserByIdUseCase } from './find-user-by-id-usecase'
-
 type MakeSutType = {
   sut: FindUserById
+  findUserByIdRepo: FindUserByIdRepo
+}
+
+class FindUserByIdRepoStub implements FindUserByIdRepo {
+  async execute (userId: string): Promise<UserModel | null> {
+    return {
+      id: 'string',
+      firstName: 'string',
+      lastName: 'string',
+      email: 'string'
+    }
+  }
 }
 
 const makeSut = (): MakeSutType => {
-  const sut: FindUserById = new FindUserByIdUseCase()
-
+  const findUserByIdRepo = new FindUserByIdRepoStub()
+  const sut = new FindUserByIdUseCase(findUserByIdRepo)
   return {
-    sut
+    sut, findUserByIdRepo
   }
 }
 
@@ -19,5 +32,12 @@ describe('FindUserByIdUserCase', () => {
     const sutSpy = jest.spyOn(sut, 'perform')
     await sut.perform({ userId: 'valid_id' })
     expect(sutSpy).toHaveBeenCalledWith({ userId: 'valid_id' })
+  })
+
+  it('should call FindUserByIdRepo with correct values', async () => {
+    const { sut, findUserByIdRepo } = makeSut()
+    const findUserByIdRepoSpy = jest.spyOn(findUserByIdRepo, 'execute')
+    await sut.perform({ userId: 'valid_id' })
+    expect(findUserByIdRepoSpy).toHaveBeenCalledWith('valid_id')
   })
 })
