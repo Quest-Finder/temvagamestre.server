083fe399da71b9a281326d27889deecec5b901c8
Felipe Souza <felipedb91@gmail.com>
2024-03-30 20:41:51 -0300

    feat: ensure FindUserByIdPrismaRepo call getPrisma function from PrismaHelper

--- src/infra/db/prisma/data/user/find-user-by-id/find-user-by-id-prisma-repo.ts
+++ src/infra/db/prisma/data/user/find-user-by-id/find-user-by-id-prisma-repo.ts
@@ -1,8 +1,10 @@
 import { type UserModel } from '@/domain/models'
+import { PrismaHelper } from '@/infra/db/prisma/helpers'
 import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
 
 export class FindUserByIdPrismaRepo implements FindUserByIdRepo {
   async execute (userId: string): Promise<UserModel | null> {
+    await PrismaHelper.getPrisma()
     return null
   }
 }
