d4c16c3a20a0e7b6f047b9ad3e2952954de085ed
Felipe Souza <felipedb91@gmail.com>
2024-03-30 21:24:52 -0300

    feat: should return user if userId exists

--- src/infra/db/prisma/data/user/find-user-by-id/find-user-by-id-prisma-repo.ts
+++ src/infra/db/prisma/data/user/find-user-by-id/find-user-by-id-prisma-repo.ts
@@ -4,7 +4,11 @@ import { type FindUserByIdRepo } from '@/usecases/contracts/db/user'
 
 export class FindUserByIdPrismaRepo implements FindUserByIdRepo {
   async execute (userId: string): Promise<UserModel | null> {
-    await PrismaHelper.getPrisma()
-    return null
+    const prisma = await PrismaHelper.getPrisma()
+    return await prisma.user.findUnique({
+      where: {
+        id: userId
+      }
+    })
   }
 }
