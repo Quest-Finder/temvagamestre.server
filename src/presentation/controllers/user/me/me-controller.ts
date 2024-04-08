09a949fff37fb93a229491d5b585b5b2795b3297
Felipe Souza <felipedb91@gmail.com>
2024-04-08 20:35:41 -0300

    feat: ensure return 500 when FindUserById throws

--- src/presentation/controllers/user/me/me-controller.ts
+++ src/presentation/controllers/user/me/me-controller.ts
@@ -1,22 +1,27 @@
 import { type FindUserById } from '@/domain/contracts/user'
 import { type Controller } from '@/presentation/contracts'
+import { serverError } from '@/presentation/helpers/http-helpers'
 import { type HttpRequest, type HttpResponse } from '@/presentation/types/http'
 
 export class MeController implements Controller {
   constructor (private readonly findUserById: FindUserById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.findUserById.perform({
-      userId: httpRequest.headers.userId
-    })
-    return await Promise.resolve({
-      statusCode: 200,
-      body: {
-        id: 'valid_id',
-        firstName: 'string',
-        lastName: 'string',
-        email: 'string'
-      }
-    })
+    try {
+      await this.findUserById.perform({
+        userId: httpRequest.headers.userId
+      })
+      return await Promise.resolve({
+        statusCode: 200,
+        body: {
+          id: 'valid_id',
+          firstName: 'string',
+          lastName: 'string',
+          email: 'string'
+        }
+      })
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
