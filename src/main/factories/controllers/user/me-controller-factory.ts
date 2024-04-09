ac4c2f1e33d4bcc8e922b2527b562cf51b789e49
Felipe Souza <felipedb91@gmail.com>
2024-04-08 21:29:56 -0300

    feat: create MeController factory

---
+++ src/main/factories/controllers/user/me-controller-factory.ts
@@ -0,0 +1,9 @@
+import { makeLogControllerDecorator } from '@/main/factories/decorators'
+import { makeFindUserByIdUseCase } from '@/main/factories/usecases/user/find-user-by-id-usecase-factory'
+import { type Controller } from '@/presentation/contracts'
+import { MeController } from '@/presentation/controllers/user/me/me-controller'
+
+export const meController = (): Controller => {
+  const controller = new MeController(makeFindUserByIdUseCase())
+  return makeLogControllerDecorator(controller)
+}
