6e7f49dcbd405193060447946e100538e7b2711e
Felipe Souza <felipedb91@gmail.com>
2024-04-26 20:43:35 -0300

    feat: should return erro if word is bad

---
+++ src/validators/zod/user/bad-work-validation/bad-word-validation.ts
@@ -0,0 +1,13 @@
+import type { Validation } from '@/presentation/contracts'
+import { left, right, type Either } from '@/shared/either'
+import Filter from 'bad-words'
+export class BadWordValidation implements Validation {
+  validate (input: any): Either<Error, void> {
+    const filter = new Filter()
+    const result = filter.isProfane(input)
+    if (result) {
+      return left(new Error('Username with bad words'))
+    }
+    return right()
+  }
+}
