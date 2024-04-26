eb754d058e7756b89dc30166d6bd64618a2a8bc8
Felipe Souza <felipedb91@gmail.com>
2024-04-26 20:07:04 -0300

    feat: ensure call all validations with correct values

--- src/presentation/controllers/user/check-username/check-username-controller.ts
+++ src/presentation/controllers/user/check-username/check-username-controller.ts
@@ -4,14 +4,17 @@ import { badRequest, noContent, serverError } from '@/presentation/helpers/http-
 import { type HttpRequest, type HttpResponse } from '@/presentation/types/http'
 
 export class CheckUsernameController implements Controller {
-  constructor (private readonly validation: Validation, private readonly checkUsernameUseCase: CheckUsername) {}
+  constructor (private readonly validations: Validation[], private readonly checkUsernameUseCase: CheckUsername) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { username } = httpRequest.params
-      const result = this.validation.validate(username)
-      if (result.isLeft()) {
-        return badRequest(result.value)
+
+      for (const validation of this.validations) {
+        const result = validation.validate(username)
+        if (result.isLeft()) {
+          return badRequest(result.value)
+        }
       }
       const useCaseResult = await this.checkUsernameUseCase.perform(username)
       if (useCaseResult.isLeft()) {
