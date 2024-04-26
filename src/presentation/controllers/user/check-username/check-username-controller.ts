137bbae10638fd7d61cc81c8dd65f47d8bd94473
Felipe Souza <felipedb91@gmail.com>
2024-04-25 21:30:28 -0300

    feat: ensure call user service with correct data

--- src/presentation/controllers/user/check-username/check-username-controller.ts
+++ src/presentation/controllers/user/check-username/check-username-controller.ts
@@ -1,16 +1,19 @@
+import { type CheckUsername } from '@/domain/contracts/user/check-username'
 import { type Controller, type Validation } from '@/presentation/contracts'
 import { badRequest, noContent, serverError } from '@/presentation/helpers/http-helpers'
 import { type HttpRequest, type HttpResponse } from '@/presentation/types/http'
 
 export class CheckUsernameController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (private readonly validation: Validation, private readonly checkUsernameUseCase: CheckUsername) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const result = this.validation.validate(httpRequest.params.username)
+      const { username } = httpRequest.params
+      const result = this.validation.validate(username)
       if (result.isLeft()) {
         return badRequest(result.value)
       }
+      await this.checkUsernameUseCase.perform(username)
       return noContent()
     } catch (error: any) {
       return serverError(error)
