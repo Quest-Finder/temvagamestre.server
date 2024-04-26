d3981e99680b5598a854af3c22075f7d71ec1aae
Felipe Souza <felipedb91@gmail.com>
2024-04-25 21:24:48 -0300

    test: ensure return bad request status if validation fails

--- src/presentation/controllers/user/check-username/check-username-controller.spec.ts
+++ src/presentation/controllers/user/check-username/check-username-controller.spec.ts
@@ -1,6 +1,7 @@
 import { type Controller } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http-helpers'
 import { type HttpRequest } from '@/presentation/types/http'
-import { right, type Either } from '@/shared/either'
+import { left, right, type Either } from '@/shared/either'
 import { type Validation } from './../../../contracts/validation'
 import { CheckUsernameController } from './check-username-controller'
 
@@ -38,5 +39,12 @@ describe('CheckUsernameController', () => {
     const validationStubSpy = jest.spyOn(validation, 'validate')
     await sut.handle(makeHttpRequest())
     expect(validationStubSpy).toHaveBeenCalledWith('valid-username')
+  })
+
+  it('should return 400 if validation fails', async () => {
+    const { sut, validation } = makeSut()
+    jest.spyOn(validation, 'validate').mockReturnValue(left(new Error('error-message')))
+    const response = await sut.handle(makeHttpRequest())
+    expect(response).toEqual(badRequest(new Error('error-message')))
   })
 })
