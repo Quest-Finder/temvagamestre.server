41c087525b13cadc09cc569e38952715e7394a42
Felipe Souza <felipedb91@gmail.com>
2024-04-27 18:58:13 -0300

    refactor: change method to get

--- src/main/routes/user/user-routes.ts
+++ src/main/routes/user/user-routes.ts
@@ -1,7 +1,7 @@
 import { adaptRoute } from '@/main/factories/adapters'
 import { makeCheckUsernameController } from '@/main/factories/controllers/user/check-username-controller-factory'
 import { makeRegisterUserController } from '@/main/factories/controllers/user/register-user-controller-factory'
-import { Controller, Post, Req, Res } from '@nestjs/common'
+import { Controller, Get, Post, Req, Res } from '@nestjs/common'
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { Request, Response } from 'express'
 import { RegisterUserRoutesDto } from './dtos/register-user-routes-dto'
@@ -25,7 +25,7 @@ export class UserRoutes {
     await adaptNest.adapt(req, res)
   }
 
-  @Post('/check-username')
+  @Get('/check-username/:username')
   async checkUserName (@Req() req: Request, @Res() res: Response): Promise<void> {
     const adaptNest = adaptRoute(makeCheckUsernameController())
     await adaptNest.adapt(req, res)
