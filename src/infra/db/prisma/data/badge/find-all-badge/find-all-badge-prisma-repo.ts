eacec9ed3e5236fc8c3f5ac5b401f92dca3feca7
Felipe Souza <felipedb91@gmail.com>
2024-05-16 20:09:38 -0300

    feat: ensure return a list with icon data

--- src/infra/db/prisma/data/badge/find-all-badge/find-all-badge-prisma-repo.ts
+++ src/infra/db/prisma/data/badge/find-all-badge/find-all-badge-prisma-repo.ts
@@ -1,8 +1,10 @@
 import { type BadgeModel } from '@/domain/models/badge/badge-model'
+import { PrismaHelper } from '@/infra/db/prisma/helpers'
 import { type FindAllBadgeRepo } from '@/usecases/contracts/db/badge/find-all-badge-repo'
 
 export class FindAllBadgePrismaRepo implements FindAllBadgeRepo {
   async execute (): Promise<BadgeModel[]> {
-
+    const prisma = await PrismaHelper.getPrisma()
+    return await prisma.badge.findMany()
   }
 }
