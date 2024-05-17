7c40379221eb3cf8bf912715a59944063af6c027
Felipe Souza <felipedb91@gmail.com>
2024-05-16 21:57:31 -0300

    test: return 500 if usecase return a error

--- src/presentation/controllers/badge/find-all-badge/find-all-badge-controller.spec.ts
+++ src/presentation/controllers/badge/find-all-badge/find-all-badge-controller.spec.ts
@@ -1,6 +1,7 @@
 import { type FindAllBadge, type FindAllBadgeResponse } from '@/domain/contracts/badge/find-all-badge'
 import { type Controller } from '@/presentation/contracts'
-import { right } from '@/shared/either'
+import { serverError } from '@/presentation/helpers/http-helpers'
+import { left, right } from '@/shared/either'
 import { FindAllBadgeController } from './find-all-badge-controller'
 
 type MakeSutType = {
@@ -24,10 +25,17 @@ const makeSut = (): MakeSutType => {
 }
 
 describe('FindAllBadgeController', () => {
-  it('should call usecase 1 time', async () => {
+  it('should call usecase one time', async () => {
     const { sut, useCase } = makeSut()
     const useCaseSpy = jest.spyOn(useCase, 'perform')
     await sut.handle({})
     expect(useCaseSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 500 if usecase return a error', async () => {
+    const { sut, useCase } = makeSut()
+    jest.spyOn(useCase, 'perform').mockResolvedValue(left(new Error('any error')))
+    const response = await sut.handle({})
+    expect(response).toEqual(serverError(new Error('Server error')))
   })
 })
