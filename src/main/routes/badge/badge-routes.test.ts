4fee962a3cb74e8817ce41bcf467afadfbe665f6
Felipe Souza <felipedb91@gmail.com>
2024-05-28 20:17:00 -0300

    test: ensure return 200 when request a badges

--- src/main/routes/badge/badge-routes.test.ts
+++ src/main/routes/badge/badge-routes.test.ts
@@ -1,18 +1,55 @@
-import { Test, type TestingModule } from '@nestjs/testing'
-import { BadgeRoutesController } from './badge-routes'
+import { PrismaHelper } from '@/infra/db/prisma/helpers'
+import { AppModule } from '@/main/app.module'
+import { type INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { type Badge, type PrismaClient } from '@prisma/client'
+import request from 'supertest'
+
+let prisma: PrismaClient
+let app: INestApplication
+
+const makeFakeBadge = (): Badge => {
+  return {
+    id: 'valid_id',
+    criteria: 'any-criteria',
+    description: 'some description',
+    icon: 'http://some-badge-url.com/badge.png',
+    name: 'some name',
+    type: 'some-type'
+  }
+}
 
 describe('BadgeRoutesController', () => {
-  let controller: BadgeRoutesController
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [BadgeRoutesController]
-    }).compile()
-
-    controller = module.get<BadgeRoutesController>(BadgeRoutesController)
+  beforeAll(async () => {
+    await PrismaHelper.connect()
+    prisma = await PrismaHelper.getPrisma()
   })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined()
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+    app = module.createNestApplication()
+    await app.init()
+  })
+
+  afterEach(async () => {
+    await prisma.badge.deleteMany()
+    await app.close()
+  })
+
+  afterAll(async () => {
+    await PrismaHelper.disconnect()
+  })
+
+  describe('POST /user/social-media', () => {
+    it('Should return 204 when adding a Social Media to the User', async () => {
+      await prisma.badge.create({
+        data: makeFakeBadge()
+      })
+      await request(app.getHttpServer())
+        .get('/badge')
+        .expect(200)
+    })
   })
 })
