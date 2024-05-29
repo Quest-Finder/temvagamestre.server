d4768a6710850e75cb147897b390e87601d7af26
Felipe Souza <felipedb91@gmail.com>
2024-05-28 22:12:18 -0300

    docs: update swagger docs in router

--- src/main/routes/badge/badge-routes.ts
+++ src/main/routes/badge/badge-routes.ts
@@ -1,11 +1,19 @@
 import { adaptRoute } from '@/main/factories/adapters'
 import { makeFindAllBadgesController } from '@/main/factories/controllers/badge/find-all-badges-controller-factory'
 import { Controller, Get, Req, Res } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { Request, Response } from 'express'
 
+@ApiTags('Badge')
 @Controller('/badge')
 export class BadgeRoutesController {
   @Get()
+  @ApiOperation({
+    summary: 'Retorna lista de badges',
+    description: 'Retorna lista de badges'
+  })
+  @ApiResponse({ status: 200, description: 'Sucesso: retorna lista de badges' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error: Erro interno do servidor' })
   async getAllBadges (@Req() req: Request, @Res() res: Response): Promise<void> {
     const adaptNest = adaptRoute(makeFindAllBadgesController())
     await adaptNest.adapt(req, res)
