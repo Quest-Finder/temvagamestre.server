6e510f02cfec63e2cdb6cb7fbad947330fe5f199
Felipe Souza <felipedb91@gmail.com>
2024-05-28 22:09:21 -0300

    refactor: update class name

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
+import { BadgeRoutesModule } from './routes/badge/badge-routes.module'
 import { CityStateRoutesModule } from './routes/city-state/city-state-routes.module'
 import { RpgStyleRoutesModule } from './routes/rpg-style/rpg-style-routes.module'
 import { SocialMediaRoutesModule } from './routes/social-media/social-media-routes.module'
 import { UserPreferenceRoutesModule } from './routes/user-preference/user-preference-routes.module'
 import { UserSocialMediaRoutesModule } from './routes/user-social-media/user-social-media-routes.module'
 import { UserRoutesModule } from './routes/user/user-routes.module'
-import { BadgeModule } from './routes/badge/badge-routes.module'
 
 @Module({
   imports: [
@@ -15,7 +15,7 @@ import { BadgeModule } from './routes/badge/badge-routes.module'
     UserPreferenceRoutesModule,
     RpgStyleRoutesModule,
     CityStateRoutesModule,
-    BadgeModule
+    BadgeRoutesModule
   ]
 })
 export class AppModule {}
--- src/main/routes/badge/badge-routes.module.ts
+++ src/main/routes/badge/badge-routes.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common'
+import { Module, RequestMethod, type MiddlewareConsumer, type NestModule } from '@nestjs/common'
 import { BadgeRoutesController } from './badge-routes'
 
 @Module({
   controllers: [BadgeRoutesController]
 })
-export class BadgeModule { }
+export class BadgeRoutesModule implements NestModule {
+  configure (consumer: MiddlewareConsumer): void {
+    consumer
+      .apply()
+      .forRoutes({ path: '/badge', method: RequestMethod.GET })
+  }
+}
