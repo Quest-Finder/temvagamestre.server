8834c7cfdea86efd255dcce45e5e3db2fb1a8177
Ágatha <agathamagnusg@hotmail.com>
2024-06-20 19:06:44 -0300

    fix: remove main file

--- jest.config.js
+++ jest.config.js
@@ -23,8 +23,8 @@ module.exports =  {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/index.ts',
     '!<rootDir>/src/shared/either.ts',
-    '!<rootDir>/src/main/{configs,adapters}/**/*.ts',
-    '!<rootDir>/src/main/main.ts',
+    '!<rootDir>/src/{configs,adapters}/**/*.ts',
+    '!<rootDir>/src/main.ts',
     '!<rootDir>/src/validators/**/*.ts',
     '!<rootDir>/src/**/contracts/**'
   ],
