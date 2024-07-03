cea6fded3539f245ad3492230a1a61e44d90ef1e
Felipe Souza <felipedb91@gmail.com>
2024-07-02 22:12:59 -0300

    test: ensure call repository 1 time

--- src/usecases/implementations/player-profile/find-all-players-profile/find-all-players-profile-usecase.spec.ts
+++ src/usecases/implementations/player-profile/find-all-players-profile/find-all-players-profile-usecase.spec.ts
@@ -1,12 +1,28 @@
-import { type FindAllPlayerProfile } from '@/contracts/player-profile/find-all-player-profile'
+import { type PlayerProfileModel } from '@/models'
+import { type FindAllPlayerProfileRepo } from '@/usecases/contracts/db/player-profile/find-all-players-profile-repo'
 import { FindAllPlayersProfileUseCase } from './find-all-players-profile-usecase'
 type MakeSutType = {
-  sut: FindAllPlayerProfile
+  sut: FindAllPlayersProfileUseCase
+  repository: FindAllPlayerProfileRepo
 }
+
+const makeFindAllPlayersProfileRepoStub = (): FindAllPlayerProfileRepo => {
+  class FindAllPlayerProfileRepoStub implements FindAllPlayerProfileRepo {
+    async execute (): Promise<PlayerProfileModel[]> {
+      return await Promise.resolve([
+        { id: 'some-id', name: 'some-name', description: 'some description' }
+      ])
+    }
+  }
+  return new FindAllPlayerProfileRepoStub()
+}
+
 const makeSut = (): MakeSutType => {
-  const sut = new FindAllPlayersProfileUseCase()
+  const repository = makeFindAllPlayersProfileRepoStub()
+  const sut = new FindAllPlayersProfileUseCase(repository)
   return {
-    sut
+    sut,
+    repository
   }
 }
 
@@ -16,5 +32,12 @@ describe('FindAllPlayersProfileUseCase', () => {
     const sutSpy = jest.spyOn(sut, 'perform')
     await sut.perform()
     expect(sutSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call FindAllPlayers repository', async () => {
+    const { sut, repository } = makeSut()
+    const repositorySpy = jest.spyOn(repository, 'execute')
+    await sut.perform()
+    expect(repositorySpy).toHaveBeenCalledTimes(1)
   })
 })
