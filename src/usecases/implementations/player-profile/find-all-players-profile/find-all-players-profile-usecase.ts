6c547d54e8fa7f12152a3250a74fa9f77cb0d96f
Felipe Souza <felipedb91@gmail.com>
2024-07-02 22:12:23 -0300

    feat: ensure call repository 1 time

--- src/usecases/implementations/player-profile/find-all-players-profile/find-all-players-profile-usecase.ts
+++ src/usecases/implementations/player-profile/find-all-players-profile/find-all-players-profile-usecase.ts
@@ -1,8 +1,12 @@
 import { type FindAllPlayerProfile } from '@/contracts/player-profile/find-all-player-profile'
 import { type PlayerProfileModel } from '@/models'
+import { type FindAllPlayerProfileRepo } from '@/usecases/contracts/db/player-profile/find-all-players-profile-repo'
 
 export class FindAllPlayersProfileUseCase implements FindAllPlayerProfile {
+  constructor (private readonly findAllPlayersRepository: FindAllPlayerProfileRepo) {}
+
   async perform (): Promise<PlayerProfileModel[]> {
+    await this.findAllPlayersRepository.execute()
     return await Promise.resolve([])
   }
 }
