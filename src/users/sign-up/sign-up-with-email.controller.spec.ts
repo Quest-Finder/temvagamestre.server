42812704c88fc59e965f8742ec86da469c8357a8
Clinton Brito <clintoncsb@gmail.com>
2024-09-11 17:52:55 -0300

    test(signup): add controller test for user creation

--- src/users/sign-up/sign-up-with-email.controller.spec.ts
+++ src/users/sign-up/sign-up-with-email.controller.spec.ts
@@ -5,6 +5,7 @@ import { SharedModule } from '@/shared/shared.module'
 
 describe('SignUpController', () => {
   let controller: SignUpController
+  let service: SignUpService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +15,30 @@ describe('SignUpController', () => {
     }).compile()
 
     controller = module.get<SignUpController>(SignUpController)
+    service = module.get<SignUpService>(SignUpService)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('/POST /user/signup/email', async () => {
+    const signUpWithEmailDto = {
+      email: 'test@example.com',
+      password: 'test123'
+    }
+
+    const token = 'some-token'
+
+    jest.spyOn(service, 'create').mockResolvedValueOnce({ token })
+
+    const result = await controller.create(signUpWithEmailDto)
+    expect(result).toBeInstanceOf(Promise)
+
+    await expect(result).resolves.toEqual({ token })
   })
 })
